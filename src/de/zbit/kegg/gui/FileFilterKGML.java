8635fecbf5334496da42405577b7c5aed8ece62c
Andreas Dräger <draeger@informatik.uni-tuebingen.de>
2010-02-19 12:54:04 +0000

    git-svn-id: svn://rarepos.cs.uni-tuebingen.de/SysBio/trunk@13 e728d8a2-de87-4acf-9dc5-8dc47f4ff2f0

---
+++ src/de/zbit/kegg/gui/FileFilterKGML.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package de.zbit.kegg.gui;
+
+import java.io.File;
+
+import javax.swing.filechooser.FileFilter;
+
+/**
+ * @author draeger
+ *
+ */
+public class FileFilterKGML extends FileFilter implements java.io.FileFilter {
+
+	/* (non-Javadoc)
+	 * @see javax.swing.filechooser.FileFilter#accept(java.io.File)
+	 */
+	public boolean accept(File f) {
+		return f.getName().toUpperCase().endsWith(".XML");
+	}
+
+	/* (non-Javadoc)
+	 * @see javax.swing.filechooser.FileFilter#getDescription()
+	 */
+	public String getDescription() {
+		return "KGML files (*.xml)";
+	}
+
+}
