90d2bca617ed59bcf0a8fb0d8245c6972fc5f510
Andreas Dräger <andreas.draeger@uni-tuebingen.de>
2010-02-22 14:48:23 +0000

    git-svn-id: svn://rarepos.cs.uni-tuebingen.de/SysBio/trunk@15 e728d8a2-de87-4acf-9dc5-8dc47f4ff2f0

--- src/de/zbit/kegg/gui/ConverterUI.java
+++ src/de/zbit/kegg/gui/ConverterUI.java
@@ -32,16 +32,24 @@ public class ConverterUI extends JFrame {
 		JFileChooser chooser = GUITools.createJFileChooser("usr.dir", false,
 				false, JFileChooser.FILES_ONLY, new FileFilterKGML());
 		if (chooser.showOpenDialog(this) == JFileChooser.APPROVE_OPTION) {
-      // TODO: Load info manager from fileSystem and give the manager to KEGG2jSBML as argument.
-      //Example: InfoManagement<String, String> manager = (InfoManagement<String, String>) KeggInfoManagement.loadFromFilesystem(filepath);
-      KEGG2jSBML k2s = new KEGG2jSBML();
-			SBMLDocument doc = k2s.Kegg2jSBML(chooser.getSelectedFile().getAbsolutePath());
-  		// TODO: Save info manager to fileSystem. Example: InfoManagement.saveToFilesystem(filepath, manager);
-			getContentPane().add(new SBMLModelSplitPane(doc.getModel(), SBMLsqueezer.getDefaultSettings()));
+			// TODO: Load info manager from fileSystem and give the manager to
+			// KEGG2jSBML as argument.
+			// Example: InfoManagement<String, String> manager =
+			// (InfoManagement<String, String>)
+			// KeggInfoManagement.loadFromFilesystem(filepath);
+			KEGG2jSBML k2s = new KEGG2jSBML();
+			SBMLDocument doc = k2s.Kegg2jSBML(chooser.getSelectedFile()
+					.getAbsolutePath());
+			// TODO: Save info manager to fileSystem. Example:
+			// InfoManagement.saveToFilesystem(filepath, manager);
+			getContentPane().add(
+					new SBMLModelSplitPane(doc.getModel(), SBMLsqueezer
+							.getDefaultSettings()));
 			setDefaultCloseOperation(EXIT_ON_CLOSE);
 			pack();
 			setVisible(true);
-		}
+		} else
+			dispose();
 	}
 
 	/**
--- src/de/zbit/util/Info.java
+++ src/de/zbit/util/Info.java
@@ -3,90 +3,103 @@ package de.zbit.util;
 import java.io.Serializable;
 
 /**
- * Improves data Management when data is associated to an id or descriptor. 
+ * Improves data Management when data is associated to an id or descriptor.
+ * 
  * @author wrzodek
  * 
- * @param <IDtype> Type of the id (int or string,...) must be comparable.
- * @param <INFOtype> Type of information (arbitrary)
+ * @param <IDtype>
+ *            Type of the id (int or string,...) must be comparable.
+ * @param <INFOtype>
+ *            Type of information (arbitrary)
  */
 @SuppressWarnings("unchecked")
-public class Info<IDtype extends Comparable, INFOtype> implements Comparable, Serializable {
-  private static final long serialVersionUID = 3592331552130670620L;
-  private IDtype identifier;
-  private INFOtype information;
-  
-  private int timesInfoAccessed=0;
-  
-  public INFOtype getInformation() {
-    /*
-     * Do NOT call this function internaly, since it remembers how often you use it. 
-     */
-    if (timesInfoAccessed!=Integer.MAX_VALUE) timesInfoAccessed++;
-    return information;
-  }
+public class Info<IDtype extends Comparable, INFOtype> implements Comparable,
+		Serializable {
+	private static final long serialVersionUID = 3592331552130670620L;
+	private IDtype identifier;
+	private INFOtype information;
 
-  public void setInformation(INFOtype information) {
-    this.information = information;
-  }
+	private int timesInfoAccessed = 0;
 
-  public IDtype getIdentifier() {
-    return identifier;
-  }
-  
-  public int getTimesInfoAccessed() {
-    return timesInfoAccessed;
-  }
-  
-  public void resetTimesInfoAccessed() {
-    timesInfoAccessed=0;
-  }
+	public INFOtype getInformation() {
+		/*
+		 * Do NOT call this function internaly, since it remembers how often you
+		 * use it.
+		 */
+		if (timesInfoAccessed != Integer.MAX_VALUE)
+			timesInfoAccessed++;
+		return information;
+	}
 
-  /**
-   * Construct a new element.
-   * @param identifier
-   * @param information - Content
-   */
-  public Info(IDtype identifier, INFOtype information) {
-    this.identifier = identifier;
-    this.information = information;
-  }
+	public void setInformation(INFOtype information) {
+		this.information = information;
+	}
 
-  @Override
-  public int compareTo(Object o) {
-    if (o instanceof Info) {
-      return identifier.compareTo(((Info)o).getIdentifier());
-    } else if (o instanceof Comparable ) {
-      try {
-        return identifier.compareTo((IDtype)o);
-      } catch (Exception e) {} // Invalid cast
-      return identifier.compareTo((Comparable)o);
-    }
-    System.err.println("Cannot compare Info to " + o);
-    return 0;
-  }
-  
-  @Override
-  public int hashCode() {
-    return getIdentifier().hashCode() + information.hashCode();
-  }
-  
-  @Override
-  public boolean equals(Object obj) {
-    try {
-      if (obj instanceof Info) return equals((Info<IDtype, INFOtype>)obj);
-    } catch (Exception e) {} // Other subtypes. Wrong cast!
-    return super.equals(obj);
-  }
-  
-  public boolean equals(Info<IDtype, INFOtype> o) {
-    if (o.getIdentifier().equals(this.getIdentifier()) && o.information.equals(this.information)) return true;
-    return false;
-  }
-  
-  @Override
-  public String toString(){
-    return "@InfoObject. ID: '" + getIdentifier().toString() + "'. Information: '" + information.toString() + "'.";
-  }
-  
-  
+	public IDtype getIdentifier() {
+		return identifier;
+	}
+
+	public int getTimesInfoAccessed() {
+		return timesInfoAccessed;
+	}
+
+	public void resetTimesInfoAccessed() {
+		timesInfoAccessed = 0;
+	}
+
+	/**
+	 * Construct a new element.
+	 * 
+	 * @param identifier
+	 * @param information
+	 *            - Content
+	 */
+	public Info(IDtype identifier, INFOtype information) {
+		this.identifier = identifier;
+		this.information = information;
+	}
+
+	// @Override
+	public int compareTo(Object o) {
+		if (o instanceof Info) {
+			return identifier.compareTo(((Info) o).getIdentifier());
+		} else if (o instanceof Comparable) {
+			try {
+				return identifier.compareTo((IDtype) o);
+			} catch (Exception e) {
+			} // Invalid cast
+			return identifier.compareTo((Comparable) o);
+		}
+		System.err.println("Cannot compare Info to " + o);
+		return 0;
+	}
+
+	@Override
+	public int hashCode() {
+		return getIdentifier().hashCode() + information.hashCode();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		try {
+			if (obj instanceof Info)
+				return equals((Info<IDtype, INFOtype>) obj);
+		} catch (Exception e) {
+		} // Other subtypes. Wrong cast!
+		return super.equals(obj);
+	}
+
+	public boolean equals(Info<IDtype, INFOtype> o) {
+		if (o.getIdentifier().equals(this.getIdentifier())
+				&& o.information.equals(this.information))
+			return true;
+		return false;
+	}
+
+	@Override
+	public String toString() {
+		return "@InfoObject. ID: '" + getIdentifier().toString()
+				+ "'. Information: '" + information.toString() + "'.";
+	}
+
 }
