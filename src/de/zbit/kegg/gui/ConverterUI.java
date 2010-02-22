a385b64d948bfd62cd6fdc1bfa18fe49832902e5
Clemens Wrzodek <clemens@germancafe.de>
2010-02-22 17:18:35 +0000

    git-svn-id: svn://rarepos.cs.uni-tuebingen.de/SysBio/trunk@18 e728d8a2-de87-4acf-9dc5-8dc47f4ff2f0

--- src/de/zbit/kegg/gui/ConverterUI.java
+++ src/de/zbit/kegg/gui/ConverterUI.java
@@ -34,21 +34,25 @@ public class ConverterUI extends JFrame {
 	public ConverterUI() {
 	  // Z:/workspace/SysBio/src/de/zbit/kegg/samplefiles/hsa00010.xml
 		super("KGML2SBMLconverter");
-		JFileChooser chooser = GUITools.createJFileChooser("usr.dir", false,
-				false, JFileChooser.FILES_ONLY, new FileFilterKGML());
+		JFileChooser chooser = GUITools.createJFileChooser("usr.dir", false, false, JFileChooser.FILES_ONLY, new FileFilterKGML());
 		if (chooser.showOpenDialog(this) == JFileChooser.APPROVE_OPTION) {
 		  
+		  // Speedup Kegg2SBML by loading alredy queried objects. Reduces network load and heavily reduces computation time.
 		  KEGG2jSBML k2s;
-		  if (new File("keggdb.dat").exists()) {
+		  if (new File("keggdb.dat").exists() && new File("keggdb.dat").length()>0) {
 		    KeggInfoManagement manager = (KeggInfoManagement) KeggInfoManagement.loadFromFilesystem("keggdb.dat");
 		    k2s = new KEGG2jSBML(manager);
 		  } else {
 			  k2s = new KEGG2jSBML();
 		  }
+		  //---
 		  
+		  // Convert Kegg File to SBML document.
 			SBMLDocument doc = k2s.Kegg2jSBML(chooser.getSelectedFile().getAbsolutePath());
 			
+			// Remember already queried objects
   	  KeggInfoManagement.saveToFilesystem("keggdb.dat", k2s.getKeggInfoManager());
+  	  //--
 			
 			getContentPane().add(new SBMLModelSplitPane(doc.getModel(), SBMLsqueezer.getDefaultSettings()));
 			setDefaultCloseOperation(EXIT_ON_CLOSE);
