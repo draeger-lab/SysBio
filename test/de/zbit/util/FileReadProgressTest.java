f40c887ffb32c7f48fd70042d9a8552f21343f43
Florian Mittag <thesentry@gmx.de>
2010-03-15 14:20:15 +0000

    updated FileReadProgress and added JUnit test cases
    git-svn-id: svn://rarepos.cs.uni-tuebingen.de/SysBio/trunk@41 e728d8a2-de87-4acf-9dc5-8dc47f4ff2f0

---
+++ test/de/zbit/util/FileReadProgressTest.java
@@ -0,0 +1,60 @@
+package de.zbit.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+
+/**
+ * NOTE: To run this from command line, go to the "bin" directory and execute:
+ * 
+ * <pre>java -cp ../lib/junit-4.3.1.jar:. org.junit.runner.JUnitCore de.zbit.util.FileReadProgressTest</pre>
+ * 
+ * @author Florian Mittag
+ */
+public class FileReadProgressTest {
+
+  public static FileReadProgress frp;
+  
+  @Before
+  public void setUp() throws Exception {
+    frp = new FileReadProgress(150);
+  }
+
+  @After
+  public void tearDown() throws Exception {
+  }
+
+  @Test
+  public void testGetSetOutputPercentage() {
+    frp.setOutputPercentage(true);
+    assertTrue(frp.getOutputPercentage());
+    frp.setOutputPercentage(false);
+    assertFalse(frp.getOutputPercentage());
+  }
+
+  @Test
+  public void testProgressSameLine() throws InterruptedException {
+    frp.setOutputPercentage(true);
+    frp.setPrintProgessInSameLine(true);
+    for( int i = 0; i < 150; i++ ) {
+      frp.progress(1);
+      Thread.sleep(30);
+    }
+    System.out.println();
+  }
+
+  @Test
+  public void testProgressNewLine() throws InterruptedException {
+    frp.setOutputPercentage(true);
+    frp.setPrintProgessInSameLine(false);
+    for( int i = 0; i < 150; i++ ) {
+      frp.progress(1);
+      Thread.sleep(30);
+    }
+    System.out.println();
+  }
+}
