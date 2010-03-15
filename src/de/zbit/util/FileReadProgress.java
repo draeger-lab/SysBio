87d1cf6af3540f134600ac8861c1d291c6019eb4
Florian Mittag <thesentry@gmx.de>
2010-03-15 14:20:15 +0000

    updated FileReadProgress and added JUnit test cases
    git-svn-id: svn://rarepos.cs.uni-tuebingen.de/SysBio/trunk@41 e728d8a2-de87-4acf-9dc5-8dc47f4ff2f0

--- src/de/zbit/util/FileReadProgress.java
+++ src/de/zbit/util/FileReadProgress.java
@@ -4,89 +4,168 @@ import java.io.File;
 import java.io.IOException;
 
 /**
- * When reading in files and processing each line (files line by line),
- * this class is handy to give a % how much is done.
- * @author wrzodek
+ * <p>When reading in files and processing each line (files line by line),
+ * this class is handy to output the percentage of how much is done.</p>
+ * 
+ * <p>NOTE: Theoretically, this class can be used for displaying more types of
+ * progress than just reading in files.</p>
+ * 
+ * @author Clemens Wrzodek
+ * @author Florian Mittag
  */
 public class FileReadProgress {
-  private long FileLength=0;
-  private long BytesRead=0;
-  private boolean outputPercentage=true;
-  private int lastOutputtedPercentage = -1;
-  private boolean printProgressInSameLine = true;
+
+  public static final String anim= "|/-\\";
+
   
+  private long fileLength=0;
+  private long bytesRead=0;
+
+  private boolean outputPercentage=true;
+  private boolean printProgressInSameLine = true;
+
+  private int lastOutputtedPercentage = -1;
+  private int progressCounter = 0;
+  
+  /**
+   * Creates a new FileReadProgress object with the file size retrieved from
+   * the given file. Additionally, it has to be specified if the progress should
+   * be written in the same line (default) or a new line for each percentage.
+   * 
+   * @param filepath  the path of the file
+   * @param printProgessInSameLine if <code>true</code>, the output will always be in the same line
+   */
   public FileReadProgress(String filepath, boolean printProgessInSameLine) {
     this(filepath);
     this.printProgressInSameLine = printProgessInSameLine;
   }
+  
+  /**
+   * Creates a new FileReadPrograss object for the given file.
+   * 
+   * @param filepath the file to be read from
+   */
   public FileReadProgress(String filepath) {
     this(new File(filepath));
   }
+  
+  /**
+   * Creates a new FileReadPrograss object for the given file.
+   * 
+   * @param file the file to be read from
+   */
   public FileReadProgress(File file) {
     this(file.length());
   }
+  
+  /**
+   * Creates a new FileReadProgress object with the given length to be 100%.
+   * 
+   * @param length the length of the file
+   */
   public FileReadProgress(long length) {
-    FileLength = length;
+    fileLength = length;
   }
 
+  /**
+   * Returns if the current percentage should be put out every time it is updated.
+   * 
+   * @return <code>true</code>, if the current percentage should be put out
+   *         every time it is updated
+   */
   public boolean getOutputPercentage() {
     return outputPercentage;
   }
+  
+  /**
+   * Specify whether the current percentage should be put out every time it is
+   * updated. <code>true</code> means, the percentage will be put out every time
+   * it is updated, <code>false</code> means, no output will be automatically
+   * generated, but {@link DiplayBar} has to be called explicitly.
+   * 
+   * @param outputPercentage
+   */
   public void setOutputPercentage(boolean outputPercentage) {
     this.outputPercentage = outputPercentage;
   }
   /**
-   * Tries to establish a "command line" progress bar. Does not work correctly if output goes to a file.
+   * Tries to establish a "command line" progress bar. Does not work correctly
+   * if output goes to a file.
    */
   public void setPrintProgessInSameLine(boolean b) {
     printProgressInSameLine = b;
   }
   
+  /**
+   * Resets all counters back to zero.
+   */
   public void reset() {
     lastOutputtedPercentage = -1;
-    BytesRead=0;
+    bytesRead=0;
+    progressCounter = 0;
   }
   
+  /**
+   * Reports that the given line has been read from the file. This implicitly
+   * adds 1 to the length of the given string to account for newline characters.
+   * 
+   * @param curLine
+   */
   public void progress(String curLine) {
-    progress(curLine.length()+1); // +1 fuer \n. Unter windows eigentlich sogar +2...
+    if (curLine != null) {
+      progress(curLine.length() + 1); // +1 for \n. For Windows possibly +2...
+    } else {
+      progress(0);
+    }
   }
+  
   public void progress(long bytesRead) {
-    BytesRead+=bytesRead;
+    this.bytesRead += bytesRead;
+    progressCounter++;
+    
+    // if percentage output should be generated
     if (outputPercentage) {
-      int perc = getPercentage();
-      if (perc!=lastOutputtedPercentage) {
-        if (!printProgressInSameLine)
+      int perc = getPercentage();   // get the current percentage
+      if ( canPrintInSameLine() ) {
+        try {
+          System.out.write(getDisplayBarString(progressCounter, perc).getBytes());
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      } else {
+        if (perc != lastOutputtedPercentage) {
           System.out.println(perc + "%");
-        else
-          try {
-            System.out.write(DisplayBar(perc).getBytes());
-            if (perc>=100 || System.console()==null) System.out.println();
-          } catch (IOException e) {e.printStackTrace();}
-        lastOutputtedPercentage = perc;
+          lastOutputtedPercentage = perc;
+        }
       }
     }
   }
   
-  public int getPercentage() {
-    return (int)Math.round((double)BytesRead/FileLength*100.0);
+  protected boolean canPrintInSameLine() {
+    return printProgressInSameLine
+           && (System.console() != null)
+           && (System.console().writer() != null);
   }
   
-  private static String DisplayBar(int perc) {
+  public int getPercentage() {
+    return (int)Math.round((double)bytesRead/fileLength*100.0);
+  }
+  
+  
+  public static String getDisplayBarString(int counter, int percent) {
     // Wenn nicht in normaler Konsole laeuft, nur % angabe.
     if (System.console()==null || System.console().writer()==null) {
-      return perc+"%";
+      return percent+"%";
     }
     
-    String anim= "|/-\\";
     StringBuilder sb = new StringBuilder();
     
-    int x = perc / 2;
+    int x = percent / 2;
     sb.append("\r[");
     for (int k = 0; k < 50; k++)
       sb.append(((x <= k) ? " " : "="));
-    sb.append("] " + anim.charAt(perc % anim.length())  + " " + perc + "%");
+    sb.append("] " + anim.charAt(counter % anim.length())  + " " + percent + "%");
     
-    return sb.toString();
-  }
-  
+    return sb.toString();    
+  }  
 }
