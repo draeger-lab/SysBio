957d3571060b6148cc5bf5a988cd09922444f713
Florian Mittag <thesentry@gmx.de>
2010-03-16 14:23:50 +0000

    added RegExpFileFilter, which accepts file whose name matches a given pattern
    git-svn-id: svn://rarepos.cs.uni-tuebingen.de/SysBio/trunk@45 e728d8a2-de87-4acf-9dc5-8dc47f4ff2f0

---
+++ src/de/zbit/io/RegExpFileFilter.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package de.zbit.io;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.util.regex.Pattern;
+
+/**
+ * FileFilter that accepts file with a filename matching the given regular
+ * expression. The directory name is ignored
+ * 
+ * @author Florian Mittag
+ */
+public class RegExpFileFilter implements FileFilter {
+
+  protected final Pattern p;
+  
+  public RegExpFileFilter(String regexp) {
+    p = Pattern.compile(regexp);
+  }
+  
+  /* (non-Javadoc)
+   * @see java.io.FileFilter#accept(java.io.File)
+   */
+  @Override
+  public boolean accept(File pathname) {
+    return p.matcher(pathname.getName()).matches();
+  }
+
+}
