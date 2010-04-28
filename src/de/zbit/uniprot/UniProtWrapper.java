d78bc54e78ecc1c4dd4af78515b58665d6019727
Finja Wrzodek <finja@wrzodek.com>
2010-04-28 15:17:18 +0000

    git-svn-id: svn://rarepos.cs.uni-tuebingen.de/SysBio/trunk@60 e728d8a2-de87-4acf-9dc5-8dc47f4ff2f0

--- src/de/zbit/uniprot/UniProtWrapper.java
+++ src/de/zbit/uniprot/UniProtWrapper.java
@@ -17,9 +17,10 @@ import de.zbit.util.ProgressBar;
 public class UniProtWrapper extends InfoManagement<String, String> {
   private static final long serialVersionUID = -1996313057043843757L;
   private WSDbfetchClient dbfetch = new WSDbfetchClient();
-  
-  public static boolean showProgress=false;
-  
+
+  public static boolean showProgress = false;
+  public static boolean fetchRenamedProteins = false;
+
   /**
    * @param listLength
    */
@@ -36,106 +37,128 @@ public class UniProtWrapper extends InfoManagement<String, String> {
 
   public static void main(String[] args) {
     UniProtWrapper uw = new UniProtWrapper();
-    
-    System.out.println(uw.getInformation("ZCH11_HUMAN"));
-    
-    String[] anfrage = new String[]{"ZCH18_HUMAN", "1433B_HUMAN", "QUATSCH", "ZCH14_HUMAN"};
-    String[] ret = uw.getInformations(anfrage);
-    
-    System.out.println("Rückgabe:");
-    for (int i=0; i<ret.length; i++) {
-      System.out.println(anfrage[i] + ":\n" + ret[i] + "\n==============================");
-    }
+
+    System.out.println(uw.getInformation("ALBU_HUMAN"));
+
+    // String[] anfrage = new String[]{"ZCH18_HUMAN", "1433B_HUMAN", "QUATSCH", "ZCH14_HUMAN"};
+    // String[] ret = uw.getInformations(anfrage);
+    //    
+    // System.out.println("Rückgabe:");
+    // for (int i=0; i<ret.length; i++) {
+    // System.out.println(anfrage[i] + ":\n" + ret[i] + "\n==============================");
+    // }
   }
-  
-  /* (non-Javadoc)
+
+  /*
+   * (non-Javadoc)
+   * 
    * @see de.zbit.util.InfoManagement#cleanupUnserializableObject()
    */
   @Override
   protected void cleanupUnserializableObject() {
-    dbfetch=null;
+    dbfetch = null;
   }
 
-  /* (non-Javadoc)
+  /*
+   * (non-Javadoc)
+   * 
    * @see de.zbit.util.InfoManagement#fetchInformation(java.lang.Comparable)
    */
   @Override
   protected String fetchInformation(String id) throws TimeoutException,
-  UnsuccessfulRetrieveException {
+            UnsuccessfulRetrieveException {
     // protNames must be comma separated uniprot ids/acs
-    if (id==null || id.isEmpty()) throw new UnsuccessfulRetrieveException();
+    if (id == null || id.isEmpty())
+      throw new UnsuccessfulRetrieveException();
 
     // fetch proteins
     String entriesStr = "";
-    int retried=0;
-    while (retried<3 && (entriesStr==null || entriesStr.isEmpty()) ) {
+    int retried = 0;
+    while (retried < 3 && (entriesStr == null || entriesStr.isEmpty())) {
       try {
-        entriesStr = dbfetch.fetchData("uniprot:" + id.toUpperCase(), "uniprot", "raw");
-        //entriesStr = dbfetch.fetchBatch("uniprot", id, "uniprot", "raw");
+        entriesStr = dbfetch.fetchData("uniprot:" + id.toUpperCase(),
+                  "uniprot", "raw");
+        // entriesStr = dbfetch.fetchBatch("uniprot", id, "uniprot", "raw");
         break;
       } catch (Exception e) {
         retried++;
       }
     }
 
-    if (retried>=3 && (entriesStr==null || entriesStr.isEmpty()))  throw new TimeoutException();
-    if (entriesStr.trim().isEmpty()) throw new UnsuccessfulRetrieveException();
+    if (retried >= 3 && (entriesStr == null || entriesStr.isEmpty()))
+      throw new TimeoutException();
+    if (entriesStr.trim().isEmpty())
+      throw new UnsuccessfulRetrieveException();
 
     return entriesStr;
   }
 
-  /* (non-Javadoc)
+  /*
+   * (non-Javadoc)
+   * 
    * @see de.zbit.util.InfoManagement#fetchMultipleInformations(IDtype[])
    */
   @Override
   protected String[] fetchMultipleInformations(String[] ids)
             throws TimeoutException, UnsuccessfulRetrieveException {
     String[] ret = new String[ids.length];
-    
-    String queryString=""; int protInQS=0; int startID =0;
+    String queryString = "";
+    int protInQS = 0;
+    int startID = 0;
     ProgressBar prog = null;
+
     if (showProgress) {
       System.out.println("Trying mass retrieve...");
       prog = new ProgressBar(ids.length);
     }
-    for (int i=0; i<ids.length; i++) {
-      queryString+=ids[i]+",";
+
+    for (int i = 0; i < ids.length; i++) {
+      queryString += ids[i] + ",";
       protInQS++;
-      if (queryString.length()>500 || protInQS==99) {
-        queryString = queryString.substring(0, queryString.length()-1); // Remove last comma.
+      if (queryString.length() > 500 || protInQS == 99) {
+        queryString = queryString.substring(0, queryString.length() - 1); // Remove last comma.
         fetchMultipleChecked(ids, ret, queryString, startID, i);
-        
+
         queryString = "";
-        protInQS=0;
-        startID=i+1;
+        protInQS = 0;
+        startID = i + 1;
       }
-      if (showProgress) prog.DisplayBar();
-    }
-    if (!queryString.isEmpty()) {
-      queryString = queryString.substring(0, queryString.length()-1); // Remove last comma.
-      fetchMultipleChecked(ids, ret, queryString, startID, ids.length-1);
-    }
-    
-    if (showProgress) {
-      int c=0;
-      for (int i=0; i<ids.length; i++)
-        if (ret[i]==null || ret[i].isEmpty()) c++;
-      System.out.println("Fixing single not mappable or not found IDs ("+c+")...");
-      prog = new ProgressBar(c);
+      if (showProgress)
+        prog.DisplayBar();
     }
 
-    
-    // Proteine, die nicht zugeordnet werden konnten, gesondert anfordern
-    for (int i=0; i<ids.length; i++) {
-      if (ret[i]==null || ret[i].isEmpty()) {
-        try {
-          ret[i] = fetchInformation(ids[i]);
-        } catch (Exception e) {ret[i]=null;}
-        if (showProgress) prog.DisplayBar();
+    if (!queryString.isEmpty()) {
+      queryString = queryString.substring(0, queryString.length() - 1); // Remove last comma.
+      fetchMultipleChecked(ids, ret, queryString, startID, ids.length - 1);
+    }
+
+    if (fetchRenamedProteins) {
+      if (showProgress) {
+        int c = 0;
+        for (int i = 0; i < ids.length; i++)
+          if (ret[i] == null || ret[i].isEmpty())
+            c++;
+        System.out.println("Fixing single not mappable or not found IDs (" + c
+                  + ")...");
+        prog = new ProgressBar(c);
+      }
+
+      // Proteine, die nicht zugeordnet werden konnten, gesondert anfordern
+      for (int i = 0; i < ids.length; i++) {
+        if (ret[i] == null || ret[i].isEmpty()) {
+          try {
+            ret[i] = fetchInformation(ids[i]);
+          } catch (Exception e) {
+            ret[i] = null;
+          }
+          if (showProgress)
+            prog.DisplayBar();
+        }
       }
     }
-    if (showProgress) System.out.println("Done.");
-    
+
+    if (showProgress)
+      System.out.println("Done.");
 
     return ret;
   }
@@ -151,45 +174,51 @@ public class UniProtWrapper extends InfoManagement<String, String> {
             String queryString, int startID, int i) {
     // fetch proteins
     String entriesStr = "";
-    int retried=0;
-    while (retried<3 && (entriesStr==null || entriesStr.isEmpty()) ) {
+    int retried = 0;
+    while (retried < 3 && (entriesStr == null || entriesStr.isEmpty())) {
       try {
-        entriesStr = dbfetch.fetchBatch("uniprot", queryString, "uniprot", "raw");
+        entriesStr = dbfetch.fetchBatch("uniprot", queryString, "uniprot",
+                  "raw");
       } catch (Exception e) {
         retried++;
       }
     }
-    if (retried>=3 && (entriesStr==null || entriesStr.isEmpty())) {
+    if (retried >= 3 && (entriesStr == null || entriesStr.isEmpty())) {
       // Try it protein by protein
       String[] splitt = queryString.split(",");
-      for (int j=0; j<splitt.length; j++) {
+      for (int j = 0; j < splitt.length; j++) {
         try {
-          ret[startID+j] = fetchInformation(splitt[j]);
+          ret[startID + j] = fetchInformation(splitt[j]);
         } catch (Exception e) {
-          ret[startID+j] = null;
+          ret[startID + j] = null;
         }
       }
-    } else {
+    }
+    else {
       // by // divided
       String[] splitt = entriesStr.split("\n//");
 
       // Idealfall: Soviele Antworten wie Anfragen
-      if ((splitt.length-1) == queryString.split(",").length) { // -1 wei letztes = "\n"
-        int j=0;
-        for (int index=startID; index<=i; index++) {
+      if ((splitt.length - 1) == queryString.split(",").length) { // -1 wei letztes = "\n"
+        int j = 0;
+        for (int index = startID; index <= i; index++) {
           ret[index] = splitt[j];
           j++;
         }
-      } else {
-        
+      }
+      else {
+
         // Ein paar Anfragen lieferten keine Antworten => Mapping.
-        for (String info: splitt) {
-          if (info.length()<=1) continue; // letzter split = "\n"
+        for (String info : splitt) {
+          if (info.length() <= 1)
+            continue; // letzter split = "\n"
           int endPos = info.indexOf("\n", info.indexOf("\nAC"));
           String toCheck = info;
-          if (endPos>0) toCheck = info.substring(0, endPos);
-          for (int index=startID; index<=i; index++) {
-            if (ret[index]!=null && !ret[index].isEmpty()) continue;
+          if (endPos > 0)
+            toCheck = info.substring(0, endPos);
+          for (int index = startID; index <= i; index++) {
+            if (ret[index] != null && !ret[index].isEmpty())
+              continue;
 
             if (toCheck.contains(ids[index])) {
               ret[index] = info;
@@ -201,7 +230,9 @@ public class UniProtWrapper extends InfoManagement<String, String> {
     }
   }
 
-  /* (non-Javadoc)
+  /*
+   * (non-Javadoc)
+   * 
    * @see de.zbit.util.InfoManagement#restoreUnserializableObject()
    */
   @Override
