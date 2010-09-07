2a3377b01bdfdbda4336b71333f3da29e97712f1
Finja Wrzodek <finja@wrzodek.com>
2010-09-07 12:35:32 +0000

    comments added
    git-svn-id: svn://rarepos.cs.uni-tuebingen.de/SysBio/trunk@141 e728d8a2-de87-4acf-9dc5-8dc47f4ff2f0

--- src/de/zbit/uniprot/UniProtWrapper.java
+++ src/de/zbit/uniprot/UniProtWrapper.java
@@ -11,7 +11,11 @@ import de.zbit.util.InfoManagement;
 import de.zbit.util.ProgressBar;
 
 /**
- * @author Finja Buechel: finja.buechel@uni-tuebingen.de
+ * @author Finja B&uml;chel
+ * 
+ * 
+ * Manages the retrival of UniProt information
+ * 
  * 
  */
 public class UniProtWrapper extends InfoManagement<String, String> {
@@ -19,23 +23,7 @@ public class UniProtWrapper extends InfoManagement<String, String> {
    * 
    */
   private static final long serialVersionUID = -1996313057043843757L;
-  /**
-   * 
-   * @param args
-   */
-  public static void main(String[] args) {
-    UniProtWrapper uw = new UniProtWrapper();
 
-    // System.out.println(uw.getInformation("ALBU_HUMAN"));
-
-    String[] anfrage = new String[]{"ZCH18_HUMAN", "1433B_HUMAN", "QUATSCH", "ZCH14_HUMAN"};
-    String[] ret = uw.getInformations(anfrage);
-       
-    System.out.println("Rückgabe:");
-    for (int i=0; i<ret.length; i++) {
-      System.out.println(anfrage[i] + ":\n" + ret[i] + "\n==============================");
-    }
-  }
   /**
    * 
    */
@@ -154,20 +142,33 @@ public class UniProtWrapper extends InfoManagement<String, String> {
       else {
 
         // Ein paar Anfragen lieferten keine Antworten => Mapping.
+        // Iterate over every uniprot block
         for (String info : splitt) {
           if (info.length() <= 1)
             continue; // letzter split = "\n"
-          int endPos = info.indexOf("\n", info.indexOf("\nAC"));
+          // Extract first two lines with ID and AC.
+          int endPos = info.indexOf("\n", info.indexOf("\nAC")+1);
           String toCheck = info;
           if (endPos > 0)
             toCheck = info.substring(0, endPos);
+          
+          // Iterate over every ID and try to map the block to the id
           for (int index = startID; index <= i; index++) {
+            // Has the id already an associated block?
             if ((ret[index] != null) && (ret[index].length() > 0))
               continue;
-
+            
+            // Does the ID or AC line contain this idText?
+            /*
+             * TODO: contains might lead to wring results. Perform an
+             *  Case insensitive word search (e.g. query: NOA_MOUSE and
+             *  result contains "ENOA_MOUSE" => result is a wrong mapping.
+             */
             if (toCheck.contains(ids[index])) {
               ret[index] = info;
-              break;
+              // Don't break here. 1:n mapping possible. With break,
+              // we would make an 1:1 mapping.
+              //break;
             }
           }
         }
@@ -253,5 +254,61 @@ public class UniProtWrapper extends InfoManagement<String, String> {
   @Override
   protected void restoreUnserializableObject() {
     dbfetch = new WSDbfetchClient();
+  }
+  
+  /**
+   * 
+   * @param args
+   */
+  public static void main(String[] args) {
+    UniProtWrapper uw = new UniProtWrapper();
+
+//    // System.out.println(uw.getInformation("ALBU_HUMAN"));
+//
+//    WSDbfetchClient wfetch = new WSDbfetchClient();
+//    try {
+//      System.out.println(wfetch.fetchData("uniprot:P17182",
+//                "uniprot", "raw"));
+//    } catch (RemoteException e) {
+//      // TODO Auto-generated catch block
+//      e.printStackTrace();
+//    } catch (ServiceException e) {
+//      // TODO Auto-generated catch block
+//      e.printStackTrace();
+//    }
+//    if(true)return;
+    String[] anfrage = new String[]{//"P62258", "Q91G88"//, "1433B_HUMAN", "QUATSCH", "ZCH14_HUMAN"
+//              "ENOA_MOUSE",
+//              "P62879",
+//              "P11142",
+//              "P35998",
+//              "P30520",
+//              "O00231",
+//              "O08553",
+//              "Q99KI0",
+//              "P80318",
+//              "Q99LX0",
+//              "Q61598",
+//              "Q62465",
+//              "Q91WD5",
+//              "P07724",
+//              "Q9D051",
+//              "P38647",
+//              "P05064",
+//              "Q8C0M9",
+//              "Q9CPY7",
+//              "Q8CAQ8",
+//              "P50516",
+//              "Q8C1B7",
+//              "Q8C1B7",
+              "P17182",
+              "P17751"
+    };
+    String[] ret = uw.getInformations(anfrage);
+       
+    System.out.println("Rückgabe:");
+    for (int i=0; i<ret.length; i++) {
+      System.out.println(anfrage[i] + ":\n" + ret[i] + "\n==============================");
+    }
   }
 }
