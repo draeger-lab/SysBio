d7f22a806d8eb968d7072901a2c3120814439711
Clemens Wrzodek <clemens@germancafe.de>
2010-10-20 13:48:21 +0000

    Added a new AbstractProgressBar implementation for JProgressBars.
    git-svn-id: svn://rarepos.cs.uni-tuebingen.de/SysBio/trunk@183 e728d8a2-de87-4acf-9dc5-8dc47f4ff2f0

---
+++ src/de/zbit/util/ProgressBarSwing.java
@@ -0,0 +1,85 @@
+/**
+ *
+ * @author wrzodek
+ */
+package de.zbit.util;
+
+import javax.swing.JProgressBar;
+
+/**
+ * ProgressBarSwing - a swing implementation that wraps the
+ * AbstractProgressBar around a JProgressBar.
+ * @author wrzodek
+ */
+public class ProgressBarSwing extends AbstractProgressBar {
+  private static final long serialVersionUID = 2754375775367568812L;
+  
+  private JProgressBar progressBar;
+  
+  /**
+   * Create a new instance of AbstractProgressBar on the
+   * given JProgressBar.
+   * @param progressBar
+   */
+  public ProgressBarSwing(JProgressBar progressBar) {
+    super();
+    setProgressBar(progressBar);
+  }
+  
+  /*=====================================
+   * Getters and Setters
+   *=====================================*/
+  
+
+  public JProgressBar getProgressBar() {
+    return progressBar;
+  }
+  public void setProgressBar(JProgressBar progressBar) {
+    this.progressBar = progressBar;
+    if (progressBar!=null) initProgressBar();
+  }
+  
+  /*=====================================
+   * ProgressBar
+   *=====================================*/
+  
+  public void initProgressBar() {
+    // initializes Progress bar
+    if (progressBar!=null && progressBar instanceof JProgressBar) {
+      ((JProgressBar)progressBar).setMaximum(100);
+      ((JProgressBar)progressBar).setMinimum(0);
+      ((JProgressBar)progressBar).setValue(0);
+      ((JProgressBar)progressBar).setStringPainted(true);
+    }
+  }
+  
+  /* (non-Javadoc)
+   * @see de.zbit.util.AbstractProgressBar#drawProgressBar(int, double, java.lang.String)
+   */
+  @Override
+  protected void drawProgressBar(int percent, double miliSecondsRemaining, String additionalText) {
+    if (progressBar instanceof JProgressBar) {
+      ((JProgressBar)progressBar).setValue(percent);
+      
+      String s = percent + "%";
+      if (miliSecondsRemaining>0) {
+        s +=" ETR: "+ Utils.getPrettyTimeString((long) miliSecondsRemaining);
+      }
+      if (additionalText!=null && additionalText.length()>0) {
+        s += " - " + additionalText;
+      }
+      
+      ((JProgressBar)progressBar).setString(s);
+    }
+  }
+
+  /* (non-Javadoc)
+   * @see de.zbit.util.AbstractProgressBar#finished()
+   */
+  @Override
+  public void finished() {
+    // Set Progressbar to 100%
+    drawProgressBar(100,0,"");
+  }
+
+}
