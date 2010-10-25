0b8be81249cd8ef93ddf5cdec20deea2dca42a56
Andreas Dräger <andreas.draeger@uni-tuebingen.de>
2010-10-25 11:24:33 +0000

    Integrated the argparser that was suggested by Florian into our framework and implemented a nice toolkit to easily implement user-defined preferences.
    git-svn-id: svn://rarepos.cs.uni-tuebingen.de/SysBio/trunk@197 e728d8a2-de87-4acf-9dc5-8dc47f4ff2f0

---
+++ src/de/zbit/util/SBPreferences.java
@@ -0,0 +1,539 @@
+/**
+ * 
+ */
+package de.zbit.util;
+
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.InvalidPropertiesFormatException;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
+import java.util.prefs.BackingStoreException;
+import java.util.prefs.Preferences;
+
+import argparser.ArgParser;
+import argparser.BooleanHolder;
+import argparser.CharHolder;
+import argparser.DoubleHolder;
+import argparser.FloatHolder;
+import argparser.IntHolder;
+import argparser.LongHolder;
+import argparser.ObjectHolder;
+import argparser.StringHolder;
+
+/**
+ * @author Andreas Dr&auml;ger
+ * @date 2010-10-24
+ */
+public class SBPreferences implements Map<Object, Object> {
+
+	/**
+	 * 
+	 * @author Andreas Dr&auml;ger
+	 * @date 2010-10-24
+	 */
+	private class Entry implements Map.Entry<Object, Object> {
+
+		/**
+		 * 
+		 */
+		private Object key, value;
+
+		/**
+		 * 
+		 * @param key
+		 * @param value
+		 */
+		public Entry(Object key, Object value) {
+			this.key = key;
+			this.value = value;
+		}
+
+		/*
+		 * (non-Javadoc)
+		 * 
+		 * @see java.util.Map.Entry#getKey()
+		 */
+		@Override
+		public Object getKey() {
+			return key;
+		}
+
+		/**
+		 * 
+		 */
+		@Override
+		public Object getValue() {
+			return value;
+		}
+
+		/*
+		 * (non-Javadoc)
+		 * 
+		 * @see java.util.Map.Entry#setValue(java.lang.Object)
+		 */
+		@Override
+		public Object setValue(Object value) {
+			return this.value = value;
+		}
+
+	}
+
+	/**
+	 * 
+	 */
+	private Properties defaults;
+
+	/**
+	 * Some {@link Class} that contains a certain number of static {@link Field}
+	 * objects of type {@link Option} and whose class name is used to address
+	 * the node in the user preferences to persist all key-value pairs of user
+	 * settings.
+	 */
+	private Class<?> keyProvider;
+
+	/**
+	 * 
+	 */
+	private Preferences prefs;
+
+	/**
+	 * 
+	 * @param keyProvider
+	 * @param relPath
+	 * @throws InvalidPropertiesFormatException
+	 * @throws IOException
+	 */
+	public SBPreferences(Class<?> keyProvider, String relPath)
+			throws InvalidPropertiesFormatException, IOException {
+		this.keyProvider = keyProvider;
+		this.prefs = Preferences.userNodeForPackage(keyProvider);
+		this.defaults = new Properties();
+		this.defaults.loadFromXML(keyProvider.getResourceAsStream(relPath));
+	}
+
+	/**
+	 * 
+	 * @param usuage
+	 * @param args
+	 */
+	public void analyzeCommandLineArguments(String usuage, String args[]) {
+		// create the parser and specify the allowed options ...
+		ArgParser parser = new ArgParser(usuage);
+		Class<?> keyProvider = getKeyProvider();
+		Map<Option, Object> options = new HashMap<Option, Object>();
+		Object fieldValue, argHolder;
+		Option option;
+		for (Field f : keyProvider.getFields()) {
+			try {
+				fieldValue = f.get(keyProvider);
+				if (fieldValue instanceof Option) {
+					option = (Option) fieldValue;
+					argHolder = option.createArgumentHolder(get(option));
+					parser.addOption(option.getSpecification(), argHolder);
+					options.put(option, argHolder);
+				}
+			} catch (Exception exc) {
+				// This my happen if there are other fields than static options
+				// in the key provider. Also happens if the wrong type of
+				// argHolder has been added or created! But this method should
+				// work fine... (I hope). You can check with -? option.
+				// Just ignore...
+			}
+		}
+		parser.matchAllArgs(args);
+		for (Option key : options.keySet()) {
+			if (key.getRequiredType().equals(Float.class)) {
+				put(key, ((FloatHolder) options.get(key)).value);
+			} else if (key.getRequiredType().equals(Double.class)) {
+				put(key, ((DoubleHolder) options.get(key)).value);
+			} else if (key.getRequiredType().equals(Short.class)) {
+				put(key, (short) ((IntHolder) options.get(key)).value);
+			} else if (key.getRequiredType().equals(Integer.class)) {
+				put(key, ((IntHolder) options.get(key)).value);
+			} else if (key.getRequiredType().equals(Long.class)) {
+				put(key, ((LongHolder) options.get(key)).value);
+			} else if (key.getRequiredType().equals(Boolean.class)) {
+				put(key, ((BooleanHolder) options.get(key)).value);
+			} else if (key.getRequiredType().equals(Character.class)) {
+				put(key, ((CharHolder) options.get(key)).value);
+			} else if (key.getRequiredType().equals(String.class)) {
+				put(key, ((StringHolder) options.get(key)).value);
+			} else {
+				put(key, ((ObjectHolder) options.get(key)).value);
+			}
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.util.Map#clear()
+	 */
+	@Override
+	public void clear() {
+		try {
+			String keys[] = prefs.keys();
+			for (int i = keys.length - 1; 0 <= i; i--) {
+				prefs.remove(keys[i]);
+			}
+		} catch (BackingStoreException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.util.Map#containsKey(java.lang.Object)
+	 */
+	@Override
+	public boolean containsKey(Object key) {
+		return keySet().contains(key);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.util.Map#containsValue(java.lang.Object)
+	 */
+	@Override
+	public boolean containsValue(Object value) {
+		for (String key : keys()) {
+			if (get(key).equals(value)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.util.Map#entrySet()
+	 */
+	@Override
+	public Set<java.util.Map.Entry<Object, Object>> entrySet() {
+		Set<Map.Entry<Object, Object>> set = new HashSet<Map.Entry<Object, Object>>();
+		for (String key : keys()) {
+			set.add(new Entry(key, get(key)));
+		}
+		return set;
+	}
+
+	/**
+	 * @throws BackingStoreException
+	 * 
+	 */
+	public void flush() throws BackingStoreException {
+		prefs.flush();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.util.Map#get(java.lang.Object)
+	 */
+	@Override
+	public String get(Object key) {
+		return getString(key);
+	}
+
+	/**
+	 * 
+	 * @param key
+	 * @return
+	 */
+	public boolean getBoolean(Object key) {
+		String k = key.toString();
+		return prefs.getBoolean(k, Boolean.parseBoolean(defaults.get(k)
+				.toString()));
+	}
+
+	/**
+	 * 
+	 * @param key
+	 * @return
+	 */
+	public double getDouble(Object key) {
+		String k = key.toString();
+		return prefs.getDouble(k, Double
+				.parseDouble(defaults.get(k).toString()));
+	}
+
+	/**
+	 * 
+	 * @param key
+	 * @return
+	 */
+	public float getFloat(Object key) {
+		String k = key.toString();
+		return prefs.getFloat(k, Float.parseFloat(defaults.get(k).toString()));
+	}
+
+	/**
+	 * 
+	 * @param key
+	 * @return
+	 */
+	public int getInt(Object key) {
+		String k = key.toString();
+		return prefs.getInt(k, Integer.parseInt(defaults.get(k).toString()));
+	}
+
+	/**
+	 * @return the keyProvider
+	 */
+	public Class<?> getKeyProvider() {
+		return keyProvider;
+	}
+
+	/**
+	 * 
+	 * @param key
+	 * @return
+	 */
+	public long getLong(Object key) {
+		String k = key.toString();
+		return prefs.getLong(k, Long.parseLong(defaults.get(k).toString()));
+	}
+
+	/**
+	 * 
+	 * @param key
+	 * @return
+	 */
+	public String getString(Object key) {
+		String k = key.toString();
+		return prefs.get(k, defaults.get(k).toString());
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.util.Map#isEmpty()
+	 */
+	@Override
+	public boolean isEmpty() {
+		return keys().length == 0;
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public String[] keys() {
+		try {
+			return prefs.keys();
+		} catch (BackingStoreException e) {
+			String keys[] = new String[defaults.size()];
+			int i = 0;
+			for (Object key : defaults.keySet()) {
+				keys[i++] = key.toString();
+			}
+			return keys;
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.util.Map#keySet()
+	 */
+	@Override
+	public Set<Object> keySet() {
+		Set<Object> set = new HashSet<Object>();
+		for (String key : keys()) {
+			set.add(key);
+		}
+		return set;
+	}
+
+	/**
+	 * 
+	 * @param key
+	 * @param value
+	 */
+	public boolean put(Object key, boolean value) {
+		String k = key.toString();
+		if (!defaults.contains(key)) {
+			defaults.put(k, Boolean.valueOf(value));
+		}
+		boolean oldValue = getBoolean(key);
+		prefs.putBoolean(k, value);
+		return oldValue;
+	}
+
+	/**
+	 * 
+	 * @param key
+	 * @param value
+	 */
+	public double put(Object key, double value) {
+		String k = key.toString();
+		if (!defaults.containsKey(key)) {
+			defaults.put(k, Double.valueOf(value));
+		}
+		double oldValue = getDouble(key);
+		prefs.putDouble(k, value);
+		return oldValue;
+	}
+
+	/**
+	 * 
+	 * @param key
+	 * @param value
+	 */
+	public float put(Object key, float value) {
+		String k = key.toString();
+		if (!defaults.containsKey(key)) {
+			defaults.put(k, Float.valueOf(value));
+		}
+		float oldValue = getFloat(key);
+		prefs.putFloat(k, value);
+		return oldValue;
+	}
+
+	/**
+	 * 
+	 * @param key
+	 * @param value
+	 */
+	public int put(Object key, int value) {
+		String k = key.toString();
+		if (!defaults.containsKey(key)) {
+			defaults.put(k, Integer.valueOf(value));
+		}
+		int oldValue = getInt(key);
+		prefs.putInt(k, value);
+		return oldValue;
+	}
+
+	/**
+	 * 
+	 * @param key
+	 * @param value
+	 */
+	public long put(Object key, long value) {
+		String k = key.toString();
+		if (!defaults.containsKey(key)) {
+			defaults.put(k, Long.valueOf(value));
+		}
+		long oldValue = getLong(key);
+		prefs.putLong(k, value);
+		return oldValue;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.util.Map#put(java.lang.Object, java.lang.Object)
+	 */
+	@Override
+	public Object put(Object key, Object value) {
+		Object o = get(key);
+		put(key, value.toString());
+		return o;
+	}
+
+	/**
+	 * 
+	 * @param key
+	 * @param value
+	 */
+	public String put(Object key, String value) {
+		String k = key.toString();
+		if (!defaults.contains(key)) {
+			defaults.put(k, value);
+		}
+		String oldValue = get(key);
+		prefs.put(k, value);
+		return oldValue;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.util.Map#putAll(java.util.Map)
+	 */
+	@Override
+	public void putAll(Map<? extends Object, ? extends Object> m) {
+		for (Object key : m.keySet()) {
+			put(key, m.get(key));
+		}
+	}
+
+	/**
+	 * Removes the key-value pair from the preferences but doesn't affect the
+	 * default values.
+	 * 
+	 * @param key
+	 */
+	public Object remove(Object key) {
+		Object o = get(key);
+		prefs.remove(key.toString());
+		return o;
+	}
+
+	/**
+	 * 
+	 * @param props
+	 * @throws BackingStoreException
+	 */
+	public void save(Properties props) throws BackingStoreException {
+		if (!defaults.equals(props)) {
+			for (Object key : props.keySet()) {
+				prefs.put(key.toString(), props.get(key).toString());
+			}
+			defaults = props;
+			prefs.flush();
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.util.Map#size()
+	 */
+	@Override
+	public int size() {
+		return keys().length;
+	}
+
+	/**
+	 * 
+	 * @throws BackingStoreException
+	 */
+	public void sync() throws BackingStoreException {
+		prefs.sync();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return prefs.toString();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.util.Map#values()
+	 */
+	@Override
+	public Collection<Object> values() {
+		Collection<Object> c = new LinkedList<Object>();
+		for (String key : keys()) {
+			c.add(get(key));
+		}
+		return c;
+	}
+}
