44f31b0b45e73571e507fa60bb2c203d94965c45
Andreas Dräger <andreas.draeger@uni-tuebingen.de>
2010-10-28 09:36:32 +0000

    This is a general implementation of a pair of two comparable things.
    git-svn-id: svn://rarepos.cs.uni-tuebingen.de/SysBio/trunk@212 e728d8a2-de87-4acf-9dc5-8dc47f4ff2f0

---
+++ src/de/zbit/util/ValuePair.java
@@ -0,0 +1,175 @@
+package de.zbit.util;
+
+import java.io.Serializable;
+
+/**
+ * A pair of two values with type parameters. This data object is useful
+ * whenever exactly two values are required for a specific task.
+ * 
+ * @author Andreas Dr&auml;ger
+ * @date 2010-09-01
+ */
+public class ValuePair<S extends Comparable<S>, T extends Comparable<T>>
+		implements Comparable<ValuePair<S, T>>, Serializable {
+
+	/**
+	 * Generated serial version identifier.
+	 */
+	private static final long serialVersionUID = -4230267902609475128L;
+	/**
+	 * 
+	 */
+	private S a;
+	/**
+	 * 
+	 */
+	private T b;
+
+	/**
+	 * 
+	 * @param a
+	 * @param b
+	 */
+	public ValuePair(S a, T b) {
+		this.setA(a);
+		this.setB(b);
+	}
+
+	/**
+	 * 
+	 * @param valuePair
+	 */
+	public ValuePair(ValuePair<S, T> valuePair) {
+		this.a = valuePair.getA();
+		this.b = valuePair.getB();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#clone()
+	 */
+	@Override
+	public ValuePair<S, T> clone() {
+		return new ValuePair<S, T>(this);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Comparable#compareTo(java.lang.Object)
+	 */
+	public int compareTo(ValuePair<S, T> v) {
+		if (equals(v)) {
+			return 0;
+		}
+		if (!isSetA()) {
+			return Integer.MIN_VALUE;
+		}
+		if (!v.isSetA()) {
+			return Integer.MAX_VALUE;
+		}
+		int comp = getA().compareTo(v.getA());
+		if (comp == 0) {
+			if (!isSetB()) {
+				return -1;
+			}
+			if (!v.isSetB()) {
+				return 1;
+			}
+			return getB().compareTo(v.getB());
+		}
+		return comp;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@SuppressWarnings("unchecked")
+	@Override
+	public boolean equals(Object o) {
+		if (o instanceof ValuePair) {
+			try {
+				ValuePair<S, T> v = (ValuePair<S, T>) o;
+				boolean equal = true;
+				equal &= isSetA() == v.isSetA();
+				equal &= isSetB() == v.isSetB();
+				if (equal && isSetA() && isSetB()) {
+					equal &= v.getA().equals(getA()) && v.getB().equals(getB());
+				}
+				return equal;
+			} catch (ClassCastException exc) {
+				return false;
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * @return the a
+	 */
+	public S getA() {
+		return a;
+	}
+
+	/**
+	 * @return the b
+	 */
+	public T getB() {
+		return b;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		return a.hashCode() + b.hashCode();
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public boolean isSetA() {
+		return a != null;
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public boolean isSetB() {
+		return b != null;
+	}
+
+	/**
+	 * @param a
+	 *            the a to set
+	 */
+	public void setA(S a) {
+		this.a = a;
+	}
+
+	/**
+	 * @param b
+	 *            the b to set
+	 */
+	public void setB(T b) {
+		this.b = b;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return String.format("[%s, %s]", getA(), getB());
+	}
+}
