6b866ad0898ac692c541f02c9f0e4e79c9c34983
Andreas Dräger <andreas.draeger@uni-tuebingen.de>
2010-10-28 13:58:32 +0000

    Removed spinner-options from GUIOptions.
    git-svn-id: svn://rarepos.cs.uni-tuebingen.de/SysBio/trunk@215 e728d8a2-de87-4acf-9dc5-8dc47f4ff2f0

--- src/de/zbit/gui/GUIOptions.java
+++ src/de/zbit/gui/GUIOptions.java
@@ -15,54 +15,35 @@ import de.zbit.util.Option;
  * @date 2010-10-22
  */
 public interface GUIOptions {
-	/**
-	 * The path to the associated configuration file, which contains one default
-	 * value for each option defined in this interface.
-	 */
-	public static final String CONFIG_FILE_LOCATION = "GUI_Configuration.xml";
-	/**
-	 * Decide whether or not SBMLsqueezer should search for updates at start-up.
-	 */
-	public static final Option CHECK_FOR_UPDATES = new Option(
-			"CHECK_FOR_UPDATES", Boolean.class,
-			"Decide whether or not SBMLsqueezer should search for updates at start-up.");
-	/**
-	 * Standard directory where user files can be found.
-	 */
-	public static final Option OPEN_DIR = new Option("OPEN_DIR", File.class,
-			"Standard directory where user files can be found.");
-	/**
-	 * Standard directory where the user may save some files.
-	 */
-	public static final Option SAVE_DIR = new Option("SAVE_DIR", File.class,
-			"Standard directory where the user may save some files.");
-	/**
-	 * Can be used in combination with = true or = false or just --gui.
-	 * Specifies whether or not a program should display its graphical user
-	 * interface.
-	 */
-	public static final Option GUI = new Option(
-			"GUI",
-			Boolean.class,
-			"Can be used in combination with = true or = false or just --gui. Specifies whether or not a program should display its graphical user interface.");
-	/**
-	 * The minimal value for JSpinners in the GUI.
-	 */
-	public static final Option SPINNER_MIN_VALUE = new Option(
-			"SPINNER_MIN_VALUE", Double.class,
-			"The minimal value for JSpinners in the GUI.");
-	/**
-	 * The maximal value for JSpinners in the GUI.
-	 */
-	public static final Option SPINNER_MAX_VALUE = new Option(
-			"SPINNER_MAX_VALUE", Double.class,
-			"The maximal value for JSpinners in the GUI.");
-	/**
-	 * This is important for the graphical user interface as it defines the step
-	 * size between two values in input masks.
-	 */
-	public static final Option SPINNER_STEP_SIZE = new Option(
-			"SPINNER_STEP_SIZE",
-			Double.class,
-			"This is important for the graphical user interface as it defines the step size between two values in input masks.");
+    /**
+     * The path to the associated configuration file, which contains one default
+     * value for each option defined in this interface.
+     */
+    public static final String CONFIG_FILE_LOCATION = "GUI_Configuration.xml";
+    /**
+     * Decide whether or not SBMLsqueezer should search for updates at start-up.
+     */
+    public static final Option CHECK_FOR_UPDATES = new Option(
+	"CHECK_FOR_UPDATES", Boolean.class,
+	"Decide whether or not SBMLsqueezer should search for updates at start-up.");
+    /**
+     * Standard directory where user files can be found.
+     */
+    public static final Option OPEN_DIR = new Option("OPEN_DIR", File.class,
+	"Standard directory where user files can be found.");
+    /**
+     * Standard directory where the user may save some files.
+     */
+    public static final Option SAVE_DIR = new Option("SAVE_DIR", File.class,
+	"Standard directory where the user may save some files.");
+    /**
+     * Can be used in combination with = true or = false or just --gui.
+     * Specifies whether or not a program should display its graphical user
+     * interface.
+     */
+    public static final Option GUI = new Option(
+	"GUI",
+	Boolean.class,
+	"Can be used in combination with = true or = false or just --gui. Specifies whether or not a program should display its graphical user interface.");
+
 }
