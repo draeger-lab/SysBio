1eb30bf248ac8e745b31cd2b936a3678bca27b9f
Andreas Dräger <andreas.draeger@uni-tuebingen.de>
2010-11-02 17:06:28 +0000

    git-svn-id: svn://rarepos.cs.uni-tuebingen.de/SysBio/trunk@232 e728d8a2-de87-4acf-9dc5-8dc47f4ff2f0

--- src/de/zbit/gui/GUIOptions.java
+++ src/de/zbit/gui/GUIOptions.java
@@ -15,35 +15,34 @@ import de.zbit.util.Option;
  * @date 2010-10-22
  */
 public interface GUIOptions {
-    /**
-     * The path to the associated configuration file, which contains one default
-     * value for each option defined in this interface.
-     */
-    public static final String CONFIG_FILE_LOCATION = "GUI_Configuration.xml";
-    /**
-     * Decide whether or not SBMLsqueezer should search for updates at start-up.
-     */
-    public static final Option CHECK_FOR_UPDATES = new Option(
-	"CHECK_FOR_UPDATES", Boolean.class,
-	"Decide whether or not SBMLsqueezer should search for updates at start-up.");
-    /**
-     * Standard directory where user files can be found.
-     */
-    public static final Option OPEN_DIR = new Option("OPEN_DIR", File.class,
-	"Standard directory where user files can be found.");
-    /**
-     * Standard directory where the user may save some files.
-     */
-    public static final Option SAVE_DIR = new Option("SAVE_DIR", File.class,
-	"Standard directory where the user may save some files.");
-    /**
-     * Can be used in combination with = true or = false or just --gui.
-     * Specifies whether or not a program should display its graphical user
-     * interface.
-     */
-    public static final Option GUI = new Option(
-	"GUI",
-	Boolean.class,
-	"Can be used in combination with = true or = false or just --gui. Specifies whether or not a program should display its graphical user interface.");
-
+	/**
+	 * The path to the associated configuration file, which contains one default
+	 * value for each option defined in this interface.
+	 */
+	public static final String CONFIG_FILE_LOCATION = "GUI_Configuration.xml";
+	/**
+	 * Decide whether or not SBMLsqueezer should search for updates at start-up.
+	 */
+	public static final Option<Boolean> CHECK_FOR_UPDATES = new Option<Boolean>(
+		"CHECK_FOR_UPDATES", Boolean.class,
+		"Decide whether or not SBMLsqueezer should search for updates at start-up.", true);
+	/**
+	 * Standard directory where user files can be found.
+	 */
+	public static final Option<File> OPEN_DIR = new Option<File>("OPEN_DIR", File.class,
+		"Standard directory where user files can be found.", System.getProperty("user.dir"));
+	/**
+	 * Standard directory where the user may save some files.
+	 */
+	public static final Option<File> SAVE_DIR = new Option<File>("SAVE_DIR", File.class,
+		"Standard directory where the user may save some files.", System.getProperty("user.dir"));
+	/**
+	 * Can be used in combination with = true or = false or just --gui. Specifies
+	 * whether or not a program should display its graphical user interface.
+	 */
+	public static final Option<Boolean> GUI = new Option<Boolean>(
+		"GUI", Boolean.class,
+		"Can be used in combination with = true or = false or just --gui. Specifies whether or not a program should display its graphical user interface.",
+		true);
+	
 }
