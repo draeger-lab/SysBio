3c2f5fd804a320b00a247acbc75a5eacc3b33c7f
Clemens Wrzodek <clemens@germancafe.de>
2010-11-10 13:46:07 +0000

    Added a few ArrayUtils.
    git-svn-id: svn://rarepos.cs.uni-tuebingen.de/SysBio/trunk@258 e728d8a2-de87-4acf-9dc5-8dc47f4ff2f0

--- src/de/zbit/util/ArrayUtils.java
+++ src/de/zbit/util/ArrayUtils.java
@@ -1,5 +1,6 @@
 package de.zbit.util;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class ArrayUtils {
@@ -17,4 +18,45 @@ public class ArrayUtils {
     }
     return d;
   }
+  
+  
+  /**
+   * Implodes the given array. (e.g. implode(["a", "b"], " - ") would return "a - b".
+   * @param <T>
+   * @param arr
+   * @param sep
+   * @return
+   */
+  public static <T> String implode(T[] arr, String sep) {
+  	if (arr==null || sep==null || arr.length<1) return null;
+  	
+  	StringBuffer ret = new StringBuffer();
+  	for (int i=0; i<arr.length-1; i++) {
+  		ret.append(arr[i]);
+  		ret.append(sep);
+  	}
+  	ret.append(arr[arr.length-1]);
+  	
+		return ret.toString();
+  }
+
+
+  /**
+   * Removes each row (first dimension) that contains only empty or null cells.
+   * @param data
+   * @return
+   */
+  public static String[][] removeEmptyRows(String[][] data) {
+    List<String[]> ret = new LinkedList<String[]>();
+    for (int i=0; i<data.length; i++) {
+      for (int j=0; j<data[i].length; j++) {
+        if (data[i]!=null && data[i][j].length()>0) {
+          ret.add(data[i]); break;
+        }
+      }
+    }
+    
+    return ret.toArray(new String[0][]);
+  }
+  
 }
