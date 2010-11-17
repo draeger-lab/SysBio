262a46111996d99197bd846f1971865c08968241
Andreas Dräger <draeger@informatik.uni-tuebingen.de>
2010-11-17 16:44:06 +0000

    Improved the properties class: It now provides many methods that already do the parsing.
    git-svn-id: svn://rarepos.cs.uni-tuebingen.de/SysBio/trunk@270 e728d8a2-de87-4acf-9dc5-8dc47f4ff2f0

--- src/de/zbit/util/prefs/SBProperties.java
+++ src/de/zbit/util/prefs/SBProperties.java
@@ -17,26 +17,26 @@ import java.util.Properties;
  * @date 2010-10-22
  */
 public class SBProperties extends Properties {
-
+	
 	/**
 	 * Generated serial version identifier
 	 */
 	private static final long serialVersionUID = 4883076517282212786L;
-
+	
 	/**
      * 
      */
 	public SBProperties() {
 		super();
 	}
-
+	
 	/**
 	 * @param defaults
 	 */
 	public SBProperties(SBProperties defaults) {
 		super(defaults);
 	}
-
+	
 	/*
 	 * (non-Javadoc)
 	 * 
@@ -46,7 +46,7 @@ public class SBProperties extends Properties {
 	public synchronized boolean contains(Object value) {
 		return super.containsValue(value.toString());
 	}
-
+	
 	/*
 	 * (non-Javadoc)
 	 * 
@@ -59,13 +59,14 @@ public class SBProperties extends Properties {
 	
 	/*
 	 * (non-Javadoc)
+	 * 
 	 * @see java.util.Hashtable#containsValue(java.lang.Object)
 	 */
 	@Override
 	public boolean containsValue(Object value) {
 		return super.containsValue(value.toString());
 	}
-
+	
 	/*
 	 * (non-Javadoc)
 	 * 
@@ -73,38 +74,170 @@ public class SBProperties extends Properties {
 	 */
 	@Override
 	public String get(Object key) {
-		Object o = super.get(key.toString()); 
+		Object o = super.get(key.toString());
 		return o != null ? o.toString() : null;
 	}
-
+	
+	/**
+	 * 
+	 * @param key
+	 * @return
+	 */
+	public boolean getBoolean(Object key) {
+		return Boolean.parseBoolean(get(key));
+	}
+	
+	/**
+	 * 
+	 * @param key
+	 * @return
+	 */
+	public boolean getBooleanProperty(Object key) {
+		return Boolean.parseBoolean(getProperty(key));
+	}
+	
 	/**
 	 * @return
 	 */
 	public SBProperties getDefaults() {
 		return (SBProperties) defaults;
 	}
-
-
+	
+	/**
+	 * 
+	 * @param key
+	 * @return
+	 */
+	public double getDouble(Object key) {
+		return Double.parseDouble(get(key));
+	}
+	
+	/**
+	 * 
+	 * @param key
+	 * @return
+	 */
+	public double getDoubleProperty(Object key) {
+		return Double.parseDouble(getProperty(key));
+	}
+	
+	/**
+	 * 
+	 * @param key
+	 * @return
+	 */
+	public float getFloat(Object key) {
+		return Float.parseFloat(get(key));
+	}
+	
+	/**
+	 * 
+	 * @param key
+	 * @return
+	 */
+	public float getFloatProperty(Object key) {
+		return Float.parseFloat(getProperty(key));
+	}
+	
+	public int getInt(Object key) {
+		return Integer.parseInt(get(key));
+	}
+	
+	public int getIntProperty(Object key) {
+		return Integer.parseInt(getProperty(key));
+	}
+	
+	/**
+	 * 
+	 * @param key
+	 * @return
+	 */
+	public long getLong(Object key) {
+		return Long.parseLong(get(key));
+	}
+	
+	/**
+	 * 
+	 * @param key
+	 * @return
+	 */
+	public long getLongProperty(Object key) {
+		return Long.parseLong(getProperty(key));
+	}
+	
+	/**
+	 * 
+	 * @param key
+	 * @return
+	 */
+	public String getProperty(Object key) {
+		return getProperty(key.toString());
+	}
+	
+	/**
+	 * 
+	 * @param key
+	 * @return
+	 */
+	public short getShort(Object key) {
+		return Short.parseShort(get(key));
+	}
+	
+	/**
+	 * 
+	 * @param key
+	 * @return
+	 */
+	public short getShortProperty(Object key) {
+		return Short.parseShort(getProperty(key));
+	}
+	
 	/**
 	 * @return
 	 */
 	public boolean isSetDefaults() {
 		return defaults != null;
 	}
-
+	
 	/**
-	 * This method ensures that only {@link String} instances can be put into
-	 * this table. This is necessary because, when writing this to XML the super
-	 * class of this object will throw exceptions for non-{@link String} keys
-	 * and values. Furthermore, this also ensures that properties can always be
-	 * found again because we only have to compare Strings no matter what is
-	 * actually put here. {@inheritDoc}
+	 * Loads all properties from this class from the public static final Options -
+	 * default values in the keyProvider.
+	 * 
+	 * @param keyProvider
+	 */
+	public void loadFromKeyProvider(Class<?> keyProvider) {
+		Object fieldValue;
+		String k;
+		
+		for (Field field : keyProvider.getFields()) {
+			try {
+				fieldValue = field.get(keyProvider);
+				if (fieldValue instanceof Option<?>) {
+					k = fieldValue.toString();
+					// Would be possible to check for already setted values.
+					//if (defaults.getProperty(k) != null) {}
+					this.put(k, ((Option<?>) fieldValue).getDefaultValue());
+				}
+			} catch (Exception exc) {
+				exc.printStackTrace(); // XXX: Remove me.
+				// ignore non-static fields
+			}
+		}
+	}
+	
+	/**
+	 * This method ensures that only {@link String} instances can be put into this
+	 * table. This is necessary because, when writing this to XML the super class
+	 * of this object will throw exceptions for non-{@link String} keys and
+	 * values. Furthermore, this also ensures that properties can always be found
+	 * again because we only have to compare Strings no matter what is actually
+	 * put here. {@inheritDoc}
 	 */
 	@Override
 	public synchronized Object put(Object key, Object value) {
 		return super.put(key.toString(), value == null ? "" : value.toString());
 	}
-
+	
 	/**
 	 * @param defaults
 	 */
@@ -115,39 +248,4 @@ public class SBProperties extends Properties {
 		this.defaults.putAll(defaults);
 	}
 	
-	/**
-	 * Loads all properties from this class from the public static
-	 * final Options - default values in the keyProvider.
-	 * @param keyProvider
-	 */
-	public void loadFromKeyProvider(Class<?> keyProvider) {
-    Object fieldValue;
-    String k;
-    
-	  for (Field field : keyProvider.getFields()) {
-      try {
-        fieldValue = field.get(keyProvider);
-        if (fieldValue instanceof Option<?>) {
-          k = fieldValue.toString();
-          // Would be possible to check for already setted values.
-          //if (defaults.getProperty(k) != null) {}
-          this.put(k, 
-          	((Option<?>)fieldValue).getDefaultValue() );
-        }
-      } catch (Exception exc) {
-        exc.printStackTrace(); // XXX: Remove me.
-        // ignore non-static fields
-      }
-    }
-	}
-
-	/**
-	 * 
-	 * @param key
-	 * @return
-	 */
-	public String getProperty(Object key) {
-		return getProperty(key.toString());
-	}
-
 }
