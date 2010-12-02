61277d926110245eea48470806051262a2ad8cbc
Andreas Dräger <andreas.draeger@uni-tuebingen.de>
2010-12-02 16:37:37 +0000

    Changed the configuration of the BaseFrame class.
    git-svn-id: svn://rarepos.cs.uni-tuebingen.de/SysBio/trunk@295 e728d8a2-de87-4acf-9dc5-8dc47f4ff2f0

--- test/de/zbit/gui/BaseGUI.java
+++ test/de/zbit/gui/BaseGUI.java
@@ -16,12 +16,7 @@ import de.zbit.util.prefs.KeyProvider;
  * @date 2010-11-28
  */
 public class BaseGUI extends BaseFrame {
-	
-	/**
-	 * 
-	 */
-	public static String APPLICATION_NAME = "Base GUI";
-	
+		
 	/**
 	 * Generated serial version identifier.
 	 */
@@ -34,108 +29,85 @@ public class BaseGUI extends BaseFrame {
 		new BaseGUI();
 	}
 	
-	/**
-	 * 
+	
+	
+	/* (non-Javadoc)
+	 * @see de.zbit.gui.BaseFrame#getLocationOfBaseActionProperties()
 	 */
+	@Override
+	protected String getLocationOfBaseActionProperties() {
+		return "BaseActionGerman.xml";
+	}
+
+
+
 	public BaseGUI() {
 		super();
 		setVisible(true);
 	}
 
-	/* (non-Javadoc)
-	 * @see de.zbit.gui.BaseFrame#exit()
-	 */
-	public void exit() {
-		System.exit(0);
-	}
-
-	/* (non-Javadoc)
-	 * @see de.zbit.gui.BaseFrame#createJToolBar()
-	 */
-	@Override
-	protected JToolBar createJToolBar() {
-		return createDefaultToolBar();
-	}
-
-	/* (non-Javadoc)
-	 * @see de.zbit.gui.BaseFrame#createMainComponent()
-	 */
-	@Override
-	protected Component createMainComponent() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	/* (non-Javadoc)
-	 * @see de.zbit.gui.BaseFrame#getCommandLineOptions()
-	 */
-	@Override
-	public Class<? extends KeyProvider>[] getCommandLineOptions() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	/* (non-Javadoc)
-	 * @see de.zbit.gui.BaseFrame#getURLAboutMessage()
-	 */
-	@Override
-	public URL getURLAboutMessage() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	/* (non-Javadoc)
-	 * @see de.zbit.gui.BaseFrame#getURLLicense()
-	 */
-	@Override
-	public URL getURLLicense() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	/* (non-Javadoc)
-	 * @see de.zbit.gui.BaseFrame#getURLOnlineHelp()
-	 */
-	@Override
-	public URL getURLOnlineHelp() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	/* (non-Javadoc)
-	 * @see de.zbit.gui.BaseFrame#closeFile()
-	 */
 	@Override
 	public void closeFile() {
 		// TODO Auto-generated method stub
 		
 	}
 
-	/* (non-Javadoc)
-	 * @see de.zbit.gui.BaseFrame#openFile()
-	 */
+	@Override
+	protected JToolBar createJToolBar() {
+		return createDefaultToolBar();
+	}
+
+	@Override
+	protected Component createMainComponent() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void exit() {
+		dispose();
+	}
+
+	@Override
+	public Class<? extends KeyProvider>[] getCommandLineOptions() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public URL getURLAboutMessage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public URL getURLLicense() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public URL getURLOnlineHelp() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 	@Override
 	public File openFile() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	/* (non-Javadoc)
-	 * @see de.zbit.gui.BaseFrame#saveFile()
-	 */
 	@Override
 	public File saveFile() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-  /* (non-Javadoc)
-   * @see de.zbit.gui.BaseFrame#getTitle()
-   */
-  @Override
-  public String getTitle() {
-    // TODO Auto-generated method stub
-    return null;
-  }
-	
+
+
+	@Override
+	public String getApplicationName() {
+		return "Simple GUI";
+	}
 }
