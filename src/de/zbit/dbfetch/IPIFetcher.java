29fe3d3bc8e2f2ac8457eb14501ba6d338d94b82
Andreas Dräger <andreas.draeger@uni-tuebingen.de>
2011-02-16 06:54:52 +0000

    Header test
    git-svn-id: svn://rarepos.cs.uni-tuebingen.de/SysBio/trunk@351 e728d8a2-de87-4acf-9dc5-8dc47f4ff2f0

--- src/de/zbit/dbfetch/IPIFetcher.java
+++ src/de/zbit/dbfetch/IPIFetcher.java
@@ -1,10 +1,24 @@
+/*
+ * $$Id:  ${file_name} ${time} ${user} $$
+ * $$URL: ${file_name} $$
+ * ---------------------------------------------------------------------
+ * This file is part of the SysBio API library.
+ *
+ * Copyright (C) 2011 by the University of Tuebingen, Germany.
+ *
+ * This library is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation. A copy of the license
+ * agreement is provided in the file named "LICENSE.txt" included with
+ * this software distribution and also available online as
+ * <http://www.gnu.org/licenses/lgpl-3.0-standalone.html>.
+ * ---------------------------------------------------------------------
+ */
 package de.zbit.dbfetch;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.logging.Level;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import de.zbit.util.LogUtil;
 
