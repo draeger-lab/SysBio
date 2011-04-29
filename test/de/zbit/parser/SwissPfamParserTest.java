77d0acb9495795e2bbb1946512f31ab7ebc4def2
Finja Wrzodek <finja@wrzodek.com>
2011-04-29 16:36:39 +0000

    Some comment changes in SwissPfamParser
    git-svn-id: svn://rarepos.cs.uni-tuebingen.de/SysBio/trunk@431 e728d8a2-de87-4acf-9dc5-8dc47f4ff2f0

--- test/de/zbit/parser/SwissPfamParserTest.java
+++ test/de/zbit/parser/SwissPfamParserTest.java
@@ -13,22 +13,22 @@ public class SwissPfamParserTest {
   public static void main(String[] args) {    
     try {
       String pfamFile = "J:/swisspfam";
-      String outTxt = "J:/prot_dom.txt", outHumanTxt = "J:/prot_dom_HUMAN.txt", outMouseTxt = "J:/prot_dom_MOUSE.txt";
-      String outZip = "J:/prot_dom.zip", outHumanZip = "prot_dom_HUMAN.zip", outMouseZip = "prot_dom_MOUSE.zip";
+      String outTxt = "H:/dbFiles/prot_dom.txt"; //, outHumanTxt = "J:/prot_dom_HUMAN.txt", outMouseTxt = "J:/prot_dom_MOUSE.txt";
+//      String outZip = "J:/prot_dom.zip", outHumanZip = "prot_dom_HUMAN.zip", outMouseZip = "prot_dom_MOUSE.zip";
       
       
       
       SwisspfamParser sp = new SwisspfamParser();
-      sp.parseFile(pfamFile, outTxt, false, false, false);
-      
-      String species = "_HUMAN";
-      sp.getSpeciesProtDomFile(outTxt, outHumanTxt, species);
-      species = "_MOUSE";
-      sp.getSpeciesProtDomFile(outTxt, outMouseTxt, species);
-      
-      SwisspfamParser.reformatProt_Dom_Files(new BufferedReader(new FileReader(outHumanTxt)), outHumanZip);
-      SwisspfamParser.reformatProt_Dom_Files(new BufferedReader(new FileReader(outMouseTxt)), outMouseZip);
-      SwisspfamParser.reformatProt_Dom_Files(new BufferedReader(new FileReader(outTxt)), outZip);
+      sp.parseFile(pfamFile, outTxt, false, true, false);
+      if(true)return;
+//      String species = "_HUMAN";
+//      sp.getSpeciesProtDomFile(outTxt, outHumanTxt, species);
+//      species = "_MOUSE";
+//      sp.getSpeciesProtDomFile(outTxt, outMouseTxt, species);
+//      
+//      SwisspfamParser.reformatProt_Dom_Files(new BufferedReader(new FileReader(outHumanTxt)), outHumanZip);
+//      SwisspfamParser.reformatProt_Dom_Files(new BufferedReader(new FileReader(outMouseTxt)), outMouseZip);
+//      SwisspfamParser.reformatProt_Dom_Files(new BufferedReader(new FileReader(outTxt)), outZip);
       
     } catch (Exception e) {
       System.err.printf("Not able to perform Swisspfam reading files entered.", e);
