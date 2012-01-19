94526d34760859b7d30870f3e77815803cb37686
Andreas Dräger <andreas.draeger@uni-tuebingen.de>
2012-01-19 08:07:37 +0000

    Added package.html files where missing and created two new SwingWorkers (or more precisely, one worker and one convenient helper class for serial worker scheduling).
    git-svn-id: svn://rarepos.cs.uni-tuebingen.de/SysBio/trunk@732 e728d8a2-de87-4acf-9dc5-8dc47f4ff2f0

---
+++ src/de/zbit/gui/SerialWorker.java
@@ -0,0 +1,125 @@
+/* $Id$
+ * $URL$
+ * ---------------------------------------------------------------------
+ * This file is part of the SysBio API library.
+ *
+ * Copyright (C) 2011 by the University of Tuebingen, Germany.
+ *
+ * This library is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation. A copy of the license
+ * agreement is provided in the file named "LICENSE.txt" included with
+ * this software distribution and also available online as
+ * <http://www.gnu.org/licenses/lgpl-3.0-standalone.html>.
+ * ---------------------------------------------------------------------
+ */
+package de.zbit.gui;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.swing.SwingWorker;
+
+/**
+ * This class schedules given instances of {@link SwingWorker} for a serial
+ * execution, i.e., it maintains a {@link List} of open tasks (one for each
+ * worker) and executes the next worker after the previous one has been
+ * completed. While doing that it forwards all {@link PropertyChangeEvent}s to
+ * listeners that are registered in this object. It is still possible to
+ * register a listener directly at some {@link SwingWorker} in this
+ * {@link SerialWorker}. Tasks that have been completed, are directly removed
+ * from this {@link SerialWorker}, but it is possible to add more tasks at
+ * running time. To this end, you can use the {@link #add(SwingWorker)} method.
+ * 
+ * @author Andreas Dr&auml;ger
+ * @since 1.1
+ * @version $Rev$
+ */
+public class SerialWorker implements PropertyChangeListener {
+	
+	private List<PropertyChangeListener> listOfPropertyChangeListeners;
+	private LinkedList<SwingWorker<?, ?>> workers;
+
+	/**
+	 * Creates a new {@link SerialWorker} that doesn't have any tasks.
+	 */
+	public SerialWorker() {
+		super();
+		this.listOfPropertyChangeListeners = new LinkedList<PropertyChangeListener>();
+		workers = new LinkedList<SwingWorker<?, ?>>();
+	}
+	
+	/**
+	 * 
+	 * @param worker
+	 */
+	public SerialWorker(SwingWorker<?, ?>... worker) {
+		this();
+		if (worker != null) {
+			for (SwingWorker<?, ?> w : worker) {
+				workers.add(w);
+			}
+		}
+	}
+	
+	/**
+	 * 
+	 * @param worker
+	 */
+	public void add(SwingWorker<?, ?> worker) {
+		workers.add(worker);
+	}
+
+	/**
+	 * 
+	 * @param listener
+	 * @return
+	 */
+	public boolean addPropertyChangeListener(PropertyChangeListener listener) {
+		return listOfPropertyChangeListeners.add(listener);
+	}
+
+	/**
+	 * 
+	 */
+	public void execute() {
+		if (!workers.isEmpty()) {
+			SwingWorker<?, ?> worker = workers.removeFirst();
+			worker.addPropertyChangeListener(this);
+			worker.execute();
+		} else {
+			firePropertyChange("done", null, null);
+		}
+	}
+	
+	/**
+	 * 
+	 * @param propertyName
+	 * @param oldValue
+	 * @param newValue
+	 */
+	public void firePropertyChange(String propertyName, Object oldValue, Object newValue) {
+		PropertyChangeEvent evt = new PropertyChangeEvent(this, propertyName, oldValue, newValue);
+		for (PropertyChangeListener listener : listOfPropertyChangeListeners) {
+			listener.propertyChange(evt);
+		}
+	}
+
+	/* (non-Javadoc)
+	 * @see java.beans.PropertyChangeListener#propertyChange(java.beans.PropertyChangeEvent)
+	 */
+	public void propertyChange(PropertyChangeEvent evt) {
+		for (PropertyChangeListener listener : listOfPropertyChangeListeners) {
+			listener.propertyChange(evt);
+		}
+		if (evt.getPropertyName().equals("done")) {
+			if (evt.getSource() instanceof SwingWorker<?, ?>) {
+				((SwingWorker<?, ?>) evt.getSource()).removePropertyChangeListener(this);
+			}
+			execute();
+		}
+	}
+	
+}
---
+++ src/de/zbit/sbml/gui/SBMLReadingTask.java
@@ -0,0 +1,158 @@
+/* $Id$
+ * $URL$
+ * ---------------------------------------------------------------------
+ * This file is part of the SysBio API library.
+ *
+ * Copyright (C) 2011 by the University of Tuebingen, Germany.
+ *
+ * This library is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation. A copy of the license
+ * agreement is provided in the file named "LICENSE.txt" included with
+ * this software distribution and also available online as
+ * <http://www.gnu.org/licenses/lgpl-3.0-standalone.html>.
+ * ---------------------------------------------------------------------
+ */
+package de.zbit.sbml.gui;
+
+import java.awt.Component;
+import java.beans.PropertyChangeListener;
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.ResourceBundle;
+import java.util.concurrent.ExecutionException;
+import java.util.logging.Logger;
+
+import javax.swing.ProgressMonitor;
+import javax.swing.ProgressMonitorInputStream;
+import javax.swing.SwingWorker;
+import javax.xml.stream.XMLStreamException;
+
+import org.sbml.jsbml.SBMLDocument;
+import org.sbml.jsbml.SBMLReader;
+import org.sbml.jsbml.util.StringTools;
+
+import de.zbit.gui.GUITools;
+import de.zbit.util.ResourceManager;
+import de.zbit.util.Timer;
+
+/**
+ * Reads an SBML file and sends the resulting {@link SBMLDocument} to assigned
+ * {@link PropertyChangeListener}s when done. In case that the reading process
+ * takes some more time, a {@link ProgressMonitor} is displayed to the user. If
+ * the user cancels the reading process, the resulting {@link SBMLDocument} will
+ * be <code>null</code>. It is hence necessary to use this
+ * {@link SBMLReadingTask} as follows:
+ * 
+ * <pre>
+ * SBMLReadingTask task = new SBMLReadingTask(file, this);
+ * task.execute();
+ * task.addPropertyChangeListener(myListener);
+ * </pre>
+ * 
+ * Then, to access the result, please do the following:
+ * 
+ * <pre>
+ * public void propertyChange(PropertyChangeEvent evt) {
+ * 	if (evt.getPropertyName().equals(&quot;done&quot;)) {
+ * 		if ((evt.getNewValue() != null) &amp;&amp; (evt.getNewValue() instanceof SBMLDocument)) {
+ * 			// call some method here with the SBMLDocument
+ * 		} else {
+ * 			// The user has canceled the reading or it was not successful.
+ * 		}
+ * 	}
+ * }
+ * </pre>
+ * 
+ * @author Andreas Dr&auml;ger
+ * @since 1.1
+ * @version $Rev$
+ */
+public class SBMLReadingTask extends SwingWorker<SBMLDocument, Void> {
+	
+	/**
+	 * For localization support.
+	 */
+	private static final transient ResourceBundle bundle = ResourceManager.getBundle("de.zbit.sbml.locales.Messages");
+	
+	/**
+	 * The stream from which the SBML content is to be read.
+	 */
+	private InputStream inputStream;
+	
+	/**
+	 * The element from the graphical user interface that invokes this task. 
+	 */
+	private Component parent;
+	
+	/**
+	 * Monitors the reading operation to the user.
+	 */
+	private ProgressMonitor progressMonitor;
+
+	/**
+	 * Memorize the given SBML file for later access, e.g., in case of successful
+	 * reading.
+	 */
+	private File sbmlFile;
+	
+	/**
+	 * 
+	 * @param sbmlFile
+	 * @param parent
+	 * @throws FileNotFoundException
+	 */
+	public SBMLReadingTask(File sbmlFile, Component parent) throws FileNotFoundException {
+		super();
+		this.parent = parent;
+		this.sbmlFile = sbmlFile;
+		ProgressMonitorInputStream pmis = new ProgressMonitorInputStream(parent,
+			String.format(bundle.getString("READING_SBML_FILE"), sbmlFile.getName()),
+			new FileInputStream(sbmlFile));
+		progressMonitor = pmis.getProgressMonitor();
+		this.inputStream = new BufferedInputStream(pmis);
+	}
+
+	/* (non-Javadoc)
+	 * @see javax.swing.SwingWorker#doInBackground()
+	 */
+	protected SBMLDocument doInBackground() throws XMLStreamException {
+		Logger logger = Logger.getLogger(SBMLReadingTask.class.getName());
+		Timer timer = new Timer();
+		try {
+			SBMLDocument doc = SBMLReader.read(inputStream);
+			logger.info(String.format(bundle.getString("READING_TIME"), StringTools.toString(timer.getAndReset(false))));
+			return doc;
+		} catch (com.ctc.wstx.exc.WstxIOException exc) {
+			logger.info(String.format(bundle.getString("CANCELING_AT_TIME"), StringTools.toString(timer.getAndReset(false))));
+			logger.fine(exc.getLocalizedMessage());
+			return null;
+		}
+	}
+	
+	/* (non-Javadoc)
+	 * @see javax.swing.SwingWorker#done()
+	 */
+	@Override
+	protected void done() {
+		progressMonitor.close();
+		try {
+			firePropertyChange("done", null, get());
+		} catch (InterruptedException exc) {
+			GUITools.showErrorMessage(parent, exc.getLocalizedMessage());
+		} catch (ExecutionException exc) {
+			GUITools.showErrorMessage(parent, exc.getLocalizedMessage());
+		}
+	}
+
+	/**
+	 * @return the sbmlFile
+	 */
+	public File getSBMLfile() {
+		return sbmlFile;
+	}
+
+}
