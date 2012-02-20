d1f763180b82756d31393181b01e4f35462a8155
Finja Wrzodek <finja@wrzodek.com>
2012-02-20 12:47:41 +0000

    Changed the name biocarta to biopax and splitted the class BioPaxTools in two classes: BioPaxConverter and BioPax2KGML. BioPax2KGML extends BioPaxConverter, and BioPaxConverter contans methods which could be used from other converters, too.
    git-svn-id: svn://rarepos.cs.uni-tuebingen.de/SysBio/trunk@790 e728d8a2-de87-4acf-9dc5-8dc47f4ff2f0

---
+++ src/de/zbit/biopax/BioPaxPathwayHolder.java
@@ -0,0 +1,136 @@
+/*
+ * $Id$
+ * $URL$
+ * ---------------------------------------------------------------------
+ * This file is part of the SysBio API library.
+ *
+ * Copyright (C) 2011 by the University of Tuebingen, Germany.
+ *
+ * This library is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation. A copy of the license
+ * agreement is provided in the file named "LICENSE.txt" included with
+ * this software distribution and also available online as
+ * <http://www.gnu.org/licenses/lgpl-3.0-standalone.html>.
+ * ---------------------------------------------------------------------
+ */
+
+package de.zbit.biopax;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.biopax.paxtools.model.level3.Entity;
+
+/**
+ * This class stores one BioPax pathway, its BioCarta id, the standard name, 
+ * and the contained entities and gene ids in {@link Set} objects 
+ * 
+ * @author Finja B&uuml;chel
+ * @version $Rev$
+ */
+public class BioPaxPathwayHolder {
+
+  Set<Entity> entities = null;
+  Set<Integer> geneIDs = null;
+  String biopaxRDFid;
+  String name = null;
+  
+  public BioPaxPathwayHolder(String id){
+    this.biopaxRDFid = id;
+  }
+  
+  public BioPaxPathwayHolder(String id, String name){
+    this(id);
+    setPathwayName(name);
+  }
+  
+  private boolean setPathwayName(String name) {
+    if(name != null && !name.isEmpty()){
+      this.name = name;
+      return true;
+    }
+    return false;
+  }
+
+  public boolean addEntity(Entity ent){
+    if(entitiesIsSet()){
+      if (!entities.contains(ent)) {
+        entities.add(ent);
+        return true;
+      }
+    }
+    return false;
+  }
+
+  private boolean entitiesIsSet() {
+   if(entities == null)
+    return createEntities();
+   else
+     return true;
+  }
+
+  private boolean createEntities() {
+    entities = new HashSet<Entity>();
+    return true;
+  }
+  
+  public String getPathwayName(){
+    return name;
+  }
+
+  public int getNoOfEntities() {
+    return entitiesIsSet()? entities.size() : 0;
+  }
+  
+  @Override
+  public boolean equals(Object obj) {
+    if (obj instanceof BioPaxPathwayHolder){
+      if(((BioPaxPathwayHolder) obj).getRDFid().equals(this.getRDFid())){
+        return true;
+      }
+      else 
+        return false;
+    } else 
+      return false;
+  }
+  
+  @Override
+  public int hashCode() {   
+    return getRDFid().hashCode()*13;
+  }
+
+  public boolean addGeneID(Integer key) {
+    if(geneIDsIsSet()){
+      return geneIDs.add(key);
+    }
+    return false;
+  }
+
+  private boolean geneIDsIsSet() {
+    if(geneIDs == null)
+      return createGeneIDs();
+     else
+       return true;
+  }
+
+  private boolean createGeneIDs() {
+    geneIDs = new HashSet<Integer>();
+    return true;
+  }
+
+  public Set<Integer> getGeneIDs() {
+    return geneIDs;
+  }
+  
+  /**
+   * 
+   * @return an empty String if the name is null
+   */
+  public String getName(){
+    return name==null ? "" : name;
+  }
+
+  public String getRDFid() {    
+    return biopaxRDFid;
+  }
+}
