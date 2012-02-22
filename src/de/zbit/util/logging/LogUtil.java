3840d7a74b932eb9ace64ef6d157b791a8fba109
Andreas Dräger <andreas.draeger@uni-tuebingen.de>
2012-02-22 17:23:04 +0000

    Improved tree and added a method to obtain the logLevel that has been defined in the launcher.
    git-svn-id: svn://rarepos.cs.uni-tuebingen.de/SysBio/trunk@805 e728d8a2-de87-4acf-9dc5-8dc47f4ff2f0

--- src/de/zbit/util/logging/LogUtil.java
+++ src/de/zbit/util/logging/LogUtil.java
@@ -43,33 +43,19 @@ public class LogUtil {
   private static String packages_to_log[];
 
   /**
-   * Initializes logging using the Java logger and with log level INFO for
-   * all classes that are in sub-packages of de.zbit.
-   * 
-   * @param packages list of packages for which the logger should be initialized
-   *                 additionally
+   * Add a new handler
+   * @param h
+   * @param packages
    */
-  public static void initializeLogging(String... packages) {
-    initializeLogging(Level.INFO, packages);
-  }
-  
-  /**
-   * Initializes logging using the Java logger and sets the given log level for
-   * all classes that are in sub-packages of de.zbit.
-   * 
-   * @param loglevel the desired log level
-   * @param packages list of packages for which the logger should be initialized
-   *                 additionally
-   */
-  public static void initializeLogging(Level logLevel, String... packages ) {
-    // initialize logging
-    //if (showLoggingConsole) {
-    //  Logging.enableLoggingConsole();
-    //}
-    packages_to_log = packages;
-    
-    changeLogLevel(logLevel, true);
+  public static void addHandler(Handler h, String... packages ) {
+    Logger.getLogger(basePackage).addHandler(h);
 
+    // additional packages to handle
+    if (packages != null) {
+      for (String s : packages) {
+        Logger.getLogger(s).addHandler(h);
+      }
+    }
   }
   
   /**
@@ -82,7 +68,7 @@ public class LogUtil {
   }
   
   /**
-   * Change the log level of all packes for those logging has
+   * Change the log level of all packages for those logging has
    * been initialized with {@link #initializeLogging(Level, String...)}.
    * 
    * @param logLevel
@@ -113,6 +99,14 @@ public class LogUtil {
   }
   
   /**
+   * 
+   * @return
+   */
+  public static Level getCurrentLogLevel() {
+  	return Logger.getLogger("").getHandlers()[0].getLevel();
+  }
+  
+  /**
    * @return packages that have been used in the last
    * call to {@link #initializeLogging(String...)}
    */
@@ -121,19 +115,32 @@ public class LogUtil {
   }
   
   /**
-   * Add a new handler
-   * @param h
-   * @param packages
+   * Initializes logging using the Java logger and sets the given log level for
+   * all classes that are in sub-packages of de.zbit.
+   * 
+   * @param loglevel the desired log level
+   * @param packages list of packages for which the logger should be initialized
+   *                 additionally
    */
-  public static void addHandler(Handler h, String... packages ) {
-    Logger.getLogger(basePackage).addHandler(h);
-
-    // additional packages to handle
-    if (packages != null) {
-      for (String s : packages) {
-        Logger.getLogger(s).addHandler(h);
-      }
-    }
+  public static void initializeLogging(Level logLevel, String... packages ) {
+    // initialize logging
+    //if (showLoggingConsole) {
+    //  Logging.enableLoggingConsole();
+    //}
+    packages_to_log = packages;
+    
+    changeLogLevel(logLevel, true);
+  }
+  
+  /**
+   * Initializes logging using the Java logger and with log level INFO for
+   * all classes that are in sub-packages of de.zbit.
+   * 
+   * @param packages list of packages for which the logger should be initialized
+   *                 additionally
+   */
+  public static void initializeLogging(String... packages) {
+    initializeLogging(Level.INFO, packages);
   }
   
   /**
