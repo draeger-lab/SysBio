26cf983b8d50eeb63aa4a9f21ea92a072d002c75
Finja Wrzodek <finja@wrzodek.com>
2012-05-04 13:32:14 +0000

    New SBGN node for empty nodes
    git-svn-id: svn://rarepos.cs.uni-tuebingen.de/SysBio/trunk@918 e728d8a2-de87-4acf-9dc5-8dc47f4ff2f0

---
+++ src/de/zbit/graph/sbgn/EmptyNode.java
@@ -0,0 +1,19 @@
+package de.zbit.graph.sbgn;
+
+import y.view.ShapeNodeRealizer;
+
+public class EmptyNode extends ShapeNodeRealizer implements SimpleCloneMarker {
+
+  @Override
+  public void setNodeIsCloned(boolean b) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public boolean isNodeCloned() {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+}
