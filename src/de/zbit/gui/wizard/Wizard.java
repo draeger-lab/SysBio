74c6e24c9b2f255f78d181da8368ebbbcb0e4584
Florian Mittag <thesentry@gmx.de>
2012-05-21 14:41:22 +0000

    corrected the wizard panel so that the "Next" button is now default button but not selected (looked confusing)
    git-svn-id: svn://rarepos.cs.uni-tuebingen.de/SysBio/trunk@947 e728d8a2-de87-4acf-9dc5-8dc47f4ff2f0

--- src/de/zbit/gui/wizard/Wizard.java
+++ src/de/zbit/gui/wizard/Wizard.java
@@ -22,8 +22,6 @@ import java.awt.Component;
 import java.awt.Dialog;
 import java.awt.Frame;
 import java.awt.Insets;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
@@ -39,7 +37,6 @@ import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JSeparator;
 import javax.swing.KeyStroke;
@@ -566,14 +563,16 @@ public class Wizard extends WindowAdapter implements PropertyChangeListener {
     cancelButton.addActionListener(wizardController);
     helpButton.addActionListener(wizardController);
     
+    // pressing the ESCAPE button triggers "Cancel"
     getDialog().getRootPane().registerKeyboardAction(
     		wizardController, 
     		CANCEL_BUTTON_ACTION_COMMAND,
     		KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), 
     		JComponent.WHEN_IN_FOCUSED_WINDOW);
     
-    nextButton.setSelected(true);
-
+    // pressing the ESCAPE button triggers "Next"/"Finish"
+    getDialog().getRootPane().setDefaultButton(nextButton);
+    
     warningPanel = new JPanel();
     warningPanel.setLayout(new BorderLayout());
     
