0cef12b7230ddb29928500047c9f5373ffb0d74f
Andreas Dräger <andreas.draeger@uni-tuebingen.de>
2012-09-03 07:42:37 +0000

    The wizard can now display further information when finishing.
    git-svn-id: svn://rarepos.cs.uni-tuebingen.de/SysBio/trunk@995 e728d8a2-de87-4acf-9dc5-8dc47f4ff2f0

---
+++ src/de/zbit/gui/wizard/WizardFinishingListener.java
@@ -0,0 +1,40 @@
+/*
+ * $Id$
+ * $URL$
+ * ---------------------------------------------------------------------
+ * This file is part of the SysBio API library.
+ *
+ * Copyright (C) 2009-2012 by the University of Tuebingen, Germany.
+ *
+ * This library is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation. A copy of the license
+ * agreement is provided in the file named "LICENSE.txt" included with
+ * this software distribution and also available online as
+ * <http://www.gnu.org/licenses/lgpl-3.0-standalone.html>.
+ * ---------------------------------------------------------------------
+ */
+package de.zbit.gui.wizard;
+
+import java.util.EventListener;
+
+/**
+ * With the help of this interface it is possible to display additional
+ * information when finishing a wizard. Note that a
+ * {@link WizardPanelDescriptor} that is supposed to display, e.g., a progress
+ * bar when finishing, must override its method
+ * {@link WizardPanelDescriptor#isImmediatelyFinishing()} in order to indicate
+ * that it does not immediately finish, but is going to display further
+ * information. It is also possible to add multiple
+ * {@link WizardFinishingListener}s to one such descriptor in order to receive a
+ * signal as soon as the wizard is done.
+ * 
+ * @author Andreas Dr&auml;ger
+ * @since 1.1
+ * @version $Rev$
+ */
+public interface WizardFinishingListener extends EventListener {
+	
+	public void wizardFinished();
+	
+}
