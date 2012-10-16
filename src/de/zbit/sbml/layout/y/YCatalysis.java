b0776436916d49ddf59ca4d0d2f4959db1dce293
Jakob Matthes <jakob.matthes@gmail.com>
2012-10-16 14:29:26 +0000

    partial implementation of EPN and text glyph methods in YLayoutBuilder, created Y* classes implementing abstract SBGNNode classes
    git-svn-id: svn://rarepos.cs.uni-tuebingen.de/SysBio/trunk@1045 e728d8a2-de87-4acf-9dc5-8dc47f4ff2f0

---
+++ src/de/zbit/sbml/layout/y/YCatalysis.java
@@ -0,0 +1,38 @@
+/*
+ * $Id$
+ * $URL$
+ * ---------------------------------------------------------------------
+ * This file is part of the SysBio API library.
+ *
+ * Copyright (C) 2009-2012 by the University of Tuebingen, Germany.
+ *
+ * This library is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation. A copy of the license
+ * agreement is provided in the file named "LICENSE.txt" included with
+ * this software distribution and also available online as
+ * <http://www.gnu.org/licenses/lgpl-3.0-standalone.html>.
+ * ---------------------------------------------------------------------
+ */
+package de.zbit.sbml.layout.y;
+
+import org.sbml.jsbml.ext.layout.CurveSegment;
+
+import de.zbit.sbml.layout.Catalysis;
+
+/**
+ * @author Jakob Matthes
+ * @version $Rev$
+ */
+public class YCatalysis implements Catalysis {
+
+	/* (non-Javadoc)
+	 * @see de.zbit.sbml.layout.SBGNArc#draw(org.sbml.jsbml.ext.layout.CurveSegment)
+	 */
+	@Override
+	public Object draw(CurveSegment curveSegment) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/de/zbit/sbml/layout/y/YUnspecifiedNode.java
@@ -0,0 +1,38 @@
+/*
+ * $Id$
+ * $URL$
+ * ---------------------------------------------------------------------
+ * This file is part of the SysBio API library.
+ *
+ * Copyright (C) 2009-2012 by the University of Tuebingen, Germany.
+ *
+ * This library is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation. A copy of the license
+ * agreement is provided in the file named "LICENSE.txt" included with
+ * this software distribution and also available online as
+ * <http://www.gnu.org/licenses/lgpl-3.0-standalone.html>.
+ * ---------------------------------------------------------------------
+ */
+package de.zbit.sbml.layout.y;
+
+import y.view.NodeRealizer;
+import de.zbit.sbml.layout.UnspecifiedNode;
+
+/**
+ * @author Jakob Matthes
+ * @version $Rev$
+ */
+public class YUnspecifiedNode extends UnspecifiedNode<NodeRealizer> {
+
+	/* (non-Javadoc)
+	 * @see de.zbit.sbml.layout.SBGNNode#draw(double, double, double, double, double, double)
+	 */
+	@Override
+	public NodeRealizer draw(double x, double y, double z, double width,
+			double height, double depth) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
