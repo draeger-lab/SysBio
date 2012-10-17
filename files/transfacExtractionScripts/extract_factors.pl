e0546724fe0f4048b3b12fe4a3d02840080866f7
Stephanie Hoffmann <stephanietschernek@googlemail.com>
2012-10-17 08:47:12 +0000

    added scripts for generating the input files of the class Transfac2Qualmodel.java
    git-svn-id: svn://rarepos.cs.uni-tuebingen.de/SysBio/trunk@1049 e728d8a2-de87-4acf-9dc5-8dc47f4ff2f0

---
+++ files/transfacExtractionScripts/extract_factors.pl
@@ -0,0 +1,117 @@
+#!/usr/bin/perl
+
+$human = 0;
+$ac = "none";
+$name = "none";
+$type = "none";
+$geneAC = "none";
+$geneName = "none";
+$bindingsites = "";
+$complexes = "";
+$interaction = "";
+$precurser = "";
+$superfamily = "none";
+$organism = "";
+
+if (length(@ARGV) < 3) {
+	print "usage: perl extract_factors.pl [factor.dat] [bindingFactor-file] [organism-file]\n";
+}
+
+
+open(IN, "<$ARGV[0]");
+open(OUT, ">$ARGV[1]");
+open(ORG, ">$ARGV[2]");
+
+print OUT "TF_AC\tTF_name\torganism\tTF_type\tbindingsites\tencoding_gene_ac\tencoding_gene_name\tcomplexes\tinteracting_factors\tprecurser\tsuperfamily\n";
+print ORG "TF_AC\tTFname\torganism\n";
+
+while (<IN>){
+	if (/\/\//) { #new entry
+		if (!($ac =~/none/)) {
+			chop($bindingsites);
+			if (length($bindingsites) < 6) {
+				$bindingsites = "none";
+			}
+			chop($complexes);
+			if (length($complexes) < 5) {
+				$complexes = "none";
+			}
+			chop($interaction);
+			if (length($interaction) < 5) {
+				$interaction = "none";
+			}
+			chop($precurser);
+			if (length($precurser) < 5) {
+				$precurser = "none";
+			}
+
+			print OUT "$ac\t$name\t$organism\t$type\t$bindingsites\t$geneAC\t$geneName\t$complexes\t$interaction\t$precurser\t$superfamily\n";
+		}
+
+		if (length($organism) < 2) {
+			$organism = "none";
+		}
+	
+		print ORG "$ac\t$name\t$organism\n";
+
+
+		#reset
+
+		$ac = "none";
+		$name = "none";
+		$type = "none";
+		$geneAC = "none";
+		$geneName = "none";
+		$bindingsites = "";
+		$complexes = "";
+		$interaction = "";
+		$precurser = "";
+		$superfamily = "none";
+		$organism = "";
+
+		print "\n"; 
+	}
+	elsif (/^AC  (T\d{5})/) {
+		print "$1\t";
+		$ac = $1;
+	}
+	elsif (/^FA  (.*)/) {
+		$name = $1;
+	}
+	elsif (/^OS  ([A-Za-z\(\)]+),.+$/) {
+		$organism = $1;
+		print $organism;
+	}
+	elsif (/^TY  (\w+)./) {
+		$type = $1;
+	}
+	elsif (/^BS  (R\d{5}); (.+)/) {
+		$bs = $1;
+		$bindingsites .= $bs.";";
+	}
+	elsif (/^GE  (G\d{6}); ([-\_ A-Za-z\d]+)[;.:].*/) {
+		$geneAC = $1;
+		$geneName = $2;
+	}
+	elsif (/^CX  (T\d{5}); .+/) {
+		$c = $1;
+		$complexes .= $c.";";
+	}
+	elsif (/^IN  (T\d{5}); (.+)$/) {
+		$in = $1;
+		$interaction .= $in.";";
+	}
+	elsif (/^ST  (T\d{5});.+/) {
+		$pre = $1;
+		$precurser .= $pre.";";
+	}
+	elsif (/^HP  (T\d{5});.+/) {
+		$superfamily = $1;
+	}
+
+}
+
+
+close(IN);
+close(OUT);
+close(ORG);
