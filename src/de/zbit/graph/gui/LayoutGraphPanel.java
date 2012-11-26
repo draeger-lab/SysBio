8e25ed98d045dc91fa03c6646a1962f4f29ff66f
Jan Rudolph <jan.daniel.rudolph@gmail.com>
2012-11-26 17:21:13 +0000

    LayoutGraphPanel - simple class for displaying graphs uses LayoutDirector for layouting Layout2GraphML - used in SBMLEditor, will hopefully removed soon SBMLCreateEdgeMode - moved to SysBio from SBMLEditor
    git-svn-id: svn://rarepos.cs.uni-tuebingen.de/SysBio/trunk@1099 e728d8a2-de87-4acf-9dc5-8dc47f4ff2f0

---
+++ src/de/zbit/graph/gui/LayoutGraphPanel.java
@@ -0,0 +1,105 @@
+/*
+ * $Id$
+ * $URL$
+ * ---------------------------------------------------------------------
+ * This file is part of the SysBio API library.
+ *
+ * Copyright (C) 2009-2012 by the University of Tuebingen, Germany.
+ *
+ * This library is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation. A copy of the license
+ * agreement is provided in the file named "LICENSE.txt" included with
+ * this software distribution and also available online as
+ * <http://www.gnu.org/licenses/lgpl-3.0-standalone.html>.
+ * ---------------------------------------------------------------------
+ */
+package de.zbit.graph.gui;
+
+import javax.swing.JPanel;
+
+import org.sbml.jsbml.ext.layout.Layout;
+
+import y.view.EditMode;
+import y.view.Graph2DView;
+import de.zbit.sbml.layout.LayoutDirector;
+import de.zbit.sbml.layout.y.ILayoutGraph;
+import de.zbit.sbml.layout.y.YLayoutAlgorithm;
+import de.zbit.sbml.layout.y.YLayoutBuilder;
+
+/**
+ * @author Jan Rudolph
+ * @version $Rev$
+ */
+/**
+ * @author Jan Rudolph
+ * @version $Rev$
+ */
+public class LayoutGraphPanel extends JPanel {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2958151300254693100L;
+	
+	/**
+	 * contains graphical information
+	 */
+	protected ILayoutGraph layoutGraph;
+	/**
+	 * enables lasting graph manipulation
+	 */
+	protected EditMode editMode;
+	/**
+	 * contains all layout information
+	 */
+	protected Layout document;
+	/**
+	 * the view of the current graph
+	 */
+	protected Graph2DView graph2DView;
+
+
+	/**
+	 * @param layout : the layout to be displayed by this panel
+	 * @param editMode : the editMode to enable lasting graph manipulation
+	 */
+	public LayoutGraphPanel(Layout layout, EditMode editMode) {
+		super();
+		this.document = layout;
+		this.editMode = editMode;
+		LayoutDirector<ILayoutGraph> director = 
+				new LayoutDirector<ILayoutGraph>(layout, new YLayoutBuilder(), new YLayoutAlgorithm());
+		director.run();
+		this.layoutGraph = director.getProduct();
+		this.graph2DView = new Graph2DView(layoutGraph.getGraph2D());
+		this.add(graph2DView);
+	}
+
+	/**
+	 * @return the layoutGraph
+	 */
+	public ILayoutGraph getLayoutGraph() {
+		return layoutGraph;
+	}
+
+	/**
+	 * @return the editMode
+	 */
+	public EditMode getEditMode() {
+		return editMode;
+	}
+	
+	/**
+	 * @return the Document
+	 */
+	public Layout getDocument() {
+		return this.document;
+	}
+
+	/**
+	 * @return the graph2DView
+	 */
+	public Graph2DView getGraph2DView() {
+		return graph2DView;
+	}
+}
