9925cb3df6a9e26c31c2d7a68acf33cb339b026a
Andreas Dräger <andreas.draeger@uni-tuebingen.de>
2013-03-14 08:59:09 +0000

    Created a new abstract super class for cases that multiple PreferencesPanels are to be displayed somehow within one Panel. Unified the classes PreferencesPanelForKeyProviders and MultiplePreferencesPanel accordingly.
    git-svn-id: svn://rarepos.cs.uni-tuebingen.de/SysBio/trunk@1228 e728d8a2-de87-4acf-9dc5-8dc47f4ff2f0

---
+++ src/de/zbit/gui/prefs/AbstractMultiplePreferencesPanel.java
@@ -0,0 +1,286 @@
+/*
+ * $Id$
+ * $URL$
+ * ---------------------------------------------------------------------
+ * This file is part of the SysBio API library.
+ *
+ * Copyright (C) 2009-2013 by the University of Tuebingen, Germany.
+ *
+ * This library is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation. A copy of the license
+ * agreement is provided in the file named "LICENSE.txt" included with
+ * this software distribution and also available online as
+ * <http://www.gnu.org/licenses/lgpl-3.0-standalone.html>.
+ * ---------------------------------------------------------------------
+ */
+package de.zbit.gui.prefs;
+
+import java.awt.event.ItemListener;
+import java.awt.event.KeyListener;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.prefs.BackingStoreException;
+import java.util.prefs.PreferenceChangeListener;
+
+import javax.swing.JScrollPane;
+import javax.swing.event.ChangeListener;
+
+import de.zbit.gui.layout.LayoutHelper;
+import de.zbit.util.prefs.KeyProvider;
+import de.zbit.util.prefs.SBPreferences;
+
+/**
+ * 
+ * @author Andreas Dr&auml;ger
+ * @since 1.1
+ * @version $Rev$
+ */
+public abstract class AbstractMultiplePreferencesPanel extends PreferencesPanel {
+	
+	/**
+	 * Generated serial version identifier.
+	 */
+	private static final long serialVersionUID = 8781298411082354285L;
+	
+	/**
+	 * A {@link Logger} for this class.
+	 */
+	private static final Logger logger = Logger.getLogger(PreferencesPanelForKeyProviders.class.getName());
+	/**
+	 * The KeyProvider, that determines this panel.
+	 */
+	protected List<Class<? extends KeyProvider>> providers;
+	/**
+	 * Title of this panel. Will be used as tab-title.
+	 */
+	protected String title;
+	/**
+	 * 
+	 */
+	private LayoutHelper layoutHelper;
+	/**
+	 * 
+	 */
+	private List<PreferencesPanelForKeyProvider> listOfPanels = new LinkedList<PreferencesPanelForKeyProvider>();
+
+	/**
+	 * 
+	 * @throws IOException
+	 */
+	public AbstractMultiplePreferencesPanel() throws IOException {
+		super();
+	}
+	
+	/**
+	 * 
+	 * @param initPanel
+	 * @throws IOException
+	 */
+	public AbstractMultiplePreferencesPanel(boolean initPanel) throws IOException {
+		super(initPanel);
+	}
+
+	/* (non-Javadoc)
+	 * @see de.zbit.gui.prefs.PreferencesPanel#accepts(java.lang.Object)
+	 */
+	public boolean accepts(Object key) {
+	  //return preferences.keySetFull().contains(key);
+	  // Preferences keyset contains all options from the package.
+	  // Better read fields of actual key-provider. These are the
+	  // Keys we want to have in the properties!!
+		if (providers != null) {
+			for (Class<? extends KeyProvider> provider: providers) {
+				try {
+					if (KeyProvider.Tools.providesOption(provider, key.toString())) {
+						return true;
+					}
+				} catch (SecurityException e) {
+					logger.log(Level.FINER, e.getMessage(), e);
+				}
+			}
+		}
+	  return false;
+	}
+
+	/* (non-Javadoc)
+	 * @see de.zbit.gui.prefs.PreferencesPanel#getTitle()
+	 */
+	public String getTitle() {
+	  return title;
+	}
+
+	/* (non-Javadoc)
+	 * @see de.zbit.gui.prefs.PreferencesPanel#isDefaultConfiguration()
+	 */
+	@Override
+	public boolean isDefaultConfiguration() {
+		for (int i = 0; i < getPreferencesPanelCount(); i++) {
+			if (!getPreferencesPanel(i).isDefaultConfiguration()) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	/* (non-Javadoc)
+	 * @see de.zbit.gui.prefs.PreferencesPanel#isUserConfiguration()
+	 */
+	@Override
+	public boolean isUserConfiguration() {
+		for (int i = 0; i < getPreferencesPanelCount(); i++) {
+			if (!getPreferencesPanel(i).isUserConfiguration()) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	/* (non-Javadoc)
+	 * @see de.zbit.gui.prefs.PreferencesPanel#persist()
+	 */
+	@Override
+	public void persist() throws BackingStoreException {
+		for (int i = 0; i < getPreferencesPanelCount(); i++) {
+			getPreferencesPanel(i).persist();
+		}
+	}
+
+	/* (non-Javadoc)
+	 * @see de.zbit.gui.prefs.PreferencesPanel#init()
+	 */
+	public void init() {
+		layoutHelper = new LayoutHelper(this);
+	  if ((providers != null) && (providers.size() > 0)) {
+	  	for (Class<? extends KeyProvider> provider : providers) {
+	  		try {
+	  			PreferencesPanelForKeyProvider settingsPanel = new PreferencesPanelForKeyProvider(provider);
+					listOfPanels.add(settingsPanel);
+					settingsPanel.setOpaque(true);
+					JScrollPane scroll = new JScrollPane(settingsPanel);
+					scroll.setOpaque(true);
+					layoutHelper.add(scroll);
+				} catch (IOException exc) {
+					logger.fine(exc.getLocalizedMessage());
+				}
+	  	}
+	  }
+	  addItemListener(this);
+	}
+
+	/* (non-Javadoc)
+	 * @see de.zbit.gui.prefs.PreferencesPanel#addChangeListener(javax.swing.event.ChangeListener)
+	 */
+	@Override
+	public void addChangeListener(ChangeListener listener) {
+		for (int i = 0; i < getPreferencesPanelCount(); i++) {
+			getPreferencesPanel(i).addChangeListener(listener);
+		}
+	}
+
+	/* (non-Javadoc)
+	 * @see de.zbit.gui.prefs.PreferencesPanel#addItemListener(java.awt.event.ItemListener)
+	 */
+	@Override
+	public void addItemListener(ItemListener listener) {
+		for (int i = 0; i < getPreferencesPanelCount(); i++) {
+			getPreferencesPanel(i).addItemListener(listener);
+		}
+	}
+
+	/* (non-Javadoc)
+	 * @see java.awt.Component#addKeyListener(java.awt.event.KeyListener)
+	 */
+	@Override
+	public void addKeyListener(KeyListener listener) {
+		for (int i = 0; i < getPreferencesPanelCount(); i++) {
+			getPreferencesPanel(i).addKeyListener(listener);
+		}
+	}
+
+	/* (non-Javadoc)
+	 * @see de.zbit.gui.prefs.PreferencesPanel#addPreferenceChangeListener(java.util.prefs.PreferenceChangeListener)
+	 */
+	@Override
+	public void addPreferenceChangeListener(PreferenceChangeListener listener) {
+		for (int i = 0; i < getPreferencesPanelCount(); i++) {
+		  getPreferencesPanel(i).addPreferenceChangeListener(listener);
+		}
+	}
+
+	/**
+	 * @param index
+	 * @return
+	 */
+	public PreferencesPanel getPreferencesPanel(int index) {
+		return listOfPanels.get(index);
+	}
+
+	/**
+	 * Gives the number of {@link PreferencesPanel}s displayed on this element.
+	 * 
+	 * @return 0 if there is no panel or if this element has not yet been
+	 *         initialized properly.
+	 */
+	public int getPreferencesPanelCount() {
+		return listOfPanels.size();
+	}
+
+	/* (non-Javadoc)
+	 * @see de.zbit.gui.prefs.PreferencesPanel#removeChangeListener(javax.swing.event.ChangeListener)
+	 */
+	@Override
+	public boolean removeChangeListener(ChangeListener listener) {
+		boolean removed = false;
+		for (int i = 0; i < getPreferencesPanelCount(); i++) {
+		  removed |= getPreferencesPanel(i).removeChangeListener(listener);
+		}
+		return removed;
+	}
+
+	/* (non-Javadoc)
+	 * @see de.zbit.gui.prefs.PreferencesPanel#removeItemListener(java.awt.event.ItemListener)
+	 */
+	@Override
+	public boolean removeItemListener(ItemListener listener) {
+		boolean removed = false;
+		for (int i = 0; i < getPreferencesPanelCount(); i++) {
+		  removed |= getPreferencesPanel(i).removeItemListener(listener);
+		}
+		return removed;
+	}
+
+	/* (non-Javadoc)
+	 * @see de.zbit.gui.prefs.PreferencesPanel#removePreferenceChangeLisener(java.util.prefs.PreferenceChangeListener)
+	 */
+	@Override
+	public void removePreferenceChangeLisener(PreferenceChangeListener listener) {
+		for (int i = 0; i < getPreferencesPanelCount(); i++) {
+		  getPreferencesPanel(i).removePreferenceChangeLisener(listener);
+		}
+	}
+
+	/* (non-Javadoc)
+	 * @see de.zbit.gui.prefs.PreferencesPanel#restoreDefaults()
+	 */
+	@Override
+	public void restoreDefaults() {
+		for (int i = 0; i < getPreferencesPanelCount(); i++) {
+			getPreferencesPanel(i).restoreDefaults();
+		}
+		validate();
+	}
+
+	/* (non-Javadoc)
+	 * @see de.zbit.gui.prefs.PreferencesPanel#loadPreferences()
+	 */
+	protected SBPreferences loadPreferences() throws IOException {
+		// This class only gathers other preferences panels.
+		// It therefore does not have any own preferences.
+	  return null;
+	}
+	
+}
--- src/de/zbit/gui/prefs/PreferencesPanelForKeyProviders.java
+++ src/de/zbit/gui/prefs/PreferencesPanelForKeyProviders.java
@@ -17,45 +17,26 @@
 package de.zbit.gui.prefs;
 
 import java.io.IOException;
-import java.text.MessageFormat;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
-import java.util.logging.Logger;
-import java.util.prefs.BackingStoreException;
+
 
 import de.zbit.util.prefs.KeyProvider;
-import de.zbit.util.prefs.SBPreferences;
-import de.zbit.util.prefs.SBProperties;
 
 /**
  * Automatically build an options panel for multiple {@link KeyProvider}s.
  * 
  * @author Clemens Wrzodek
+ * @author Andreas Dr&auml;ger
  * @version $Rev$
  */
-public class PreferencesPanelForKeyProviders extends PreferencesPanel {
+public class PreferencesPanelForKeyProviders extends AbstractMultiplePreferencesPanel {
   
   /**
    * Generated serial version identifer.
    */
   private static final long serialVersionUID = -921160153764089395L;
   
-  /**
-   * A {@link Logger} for this class.
-   */
-  private static final Logger logger = Logger.getLogger(PreferencesPanelForKeyProviders.class.getName());
-  
-  /**
-   * The KeyProvider, that determines this panel.
-   */
-  protected List<Class<? extends KeyProvider>> providers;
-  
-  /**
-   * Title of this panel. Will be used as tab-title.
-   */
-  private String title;
-
   /**
    * 
    * @param provider
@@ -98,101 +79,6 @@ public class PreferencesPanelForKeyProviders extends PreferencesPanel {
     }
     this.title = title;
     initializePrefPanel();
-  }
-
-  /* (non-Javadoc)
-   * @see de.zbit.gui.prefs.PreferencesPanel#accepts(java.lang.Object)
-   */
-  public boolean accepts(Object key) {
-    //return preferences.keySetFull().contains(key);
-    // Preferences keyset contains all options from the package.
-    // Better read fields of actual key-provider. These are the
-    // Keys we want to have in the properties!!
-    
-    for (Class<? extends KeyProvider> provider: providers) {
-      try {
-        boolean b = (provider.getField(key.toString())!=null);
-        if (b) return true;
-      } catch (SecurityException e) {
-      } catch (NoSuchFieldException e) {}
-    }
-    return false;
-  }
-  
-  /* (non-Javadoc)
-   * @see de.zbit.gui.prefs.PreferencesPanel#getTitle()
-   */
-  public String getTitle() {
-    return title;
-  }
-  
-  /* (non-Javadoc)
-   * @see de.zbit.gui.prefs.PreferencesPanel#init()
-   */
-  @SuppressWarnings("unchecked")
-  public void init() {
-    if ((providers != null) && (providers.size() > 0)) {
-      autoBuildPanel((Class<? extends KeyProvider>[]) providers.toArray(new Class[0]));
-    }
-  }
-  
-  /* (non-Javadoc)
-   * @see de.zbit.gui.prefs.PreferencesPanel#initializePrefPanel()
-   */
-  protected void initializePrefPanel() throws IOException {
-  	String k;
-    properties = new SBProperties(new SBProperties());
-    for (Class<? extends KeyProvider> provider: providers) {
-    	preferences = SBPreferences.getPreferencesFor(provider);
-    	// TODO: In cases that there is an old configuration of preferences in memory it might be necessary to call SBPreferences.getPreferencesFor(myKeyProvider).flush() before all options can be displayed. The reason is that here all keys are taken from the preferences, instead using the actual options from the keyprovider!
-    	for (Object key : preferences.keySetFull()) {
-    		if (accepts(key)) {
-    			// Accept only key from KeyProvider!
-    			// Don't put all keys in properties (also not in preferences) here!
-    			k = key.toString();
-    			properties.put(k, preferences.get(key));
-    			properties.getDefaults().put(k, preferences.getDefault(key));
-    		} else {
-    			log.finer(MessageFormat.format("Rejecting key: {0}", key));
-    		}
-    	}
-    }
-    preferences = null;
-    init();
-  }
-  
-  /* (non-Javadoc)
-   * @see de.zbit.gui.prefs.PreferencesPanel#persist()
-   */
-  @Override
-  public void persist() throws BackingStoreException {
-    SBPreferences prefs = null;
-    for (Class<? extends KeyProvider> provider: providers) {
-      prefs = SBPreferences.getPreferencesFor(provider);
-      
-      boolean changes = false;
-      for (Map.Entry<? extends Object, ? extends Object> entry : properties.entrySet()) {
-        if (prefs.containsKey(entry.getKey()) || KeyProvider.Tools.providesOption(provider, entry.getKey().toString())) {
-        	// TODO: this is a very simple check. What if we have a new key-value pair? Null values??
-          prefs.put(entry.getKey(), entry.getValue());
-          changes = true;
-          logger.finest("Storing key-value pair (" + entry.getKey() + ", " + entry.getValue() + ") for KeyProvider " + provider.getName());
-        } else {
-        	logger.finest("Could not store key-value pair (" + entry.getKey() + ", " + entry.getValue() + ") for KeyProvider " + provider.getName());
-        }
-      }
-      
-      if (changes) {
-        prefs.flush();
-      }
-    }
-  }
-  
-  /* (non-Javadoc)
-   * @see de.zbit.gui.prefs.PreferencesPanel#loadPreferences()
-   */
-  protected SBPreferences loadPreferences() throws IOException {
-    return null;
   }
 
 }
