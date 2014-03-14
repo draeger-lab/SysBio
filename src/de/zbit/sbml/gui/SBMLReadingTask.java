4bdf3f459408361e21bc038c5fd9797d969cd462
Andreas Dräger <andreas.draeger@uni-tuebingen.de>
2014-03-14 19:16:20 +0000

    Updated the reading task for SBML to throw more meaningful errors and changed the GADGET configuration file template for Garuda.
    git-svn-id: svn://rarepos.cs.uni-tuebingen.de/SysBio/trunk@1319 e728d8a2-de87-4acf-9dc5-8dc47f4ff2f0

--- src/de/zbit/sbml/gui/SBMLReadingTask.java
+++ src/de/zbit/sbml/gui/SBMLReadingTask.java
@@ -64,7 +64,7 @@ import de.zbit.util.Timer;
  * 	if (evt.getPropertyName().equals(SBMLReadingTask.SBML_READING_SUCCESSFULLY_DONE)) {
  * 		if ((evt.getNewValue() != null)
  * 				&amp;&amp; (evt.getNewValue() instanceof OpenedFile)) {
- *       // call some method here with the OpenedFile containing 
+ *       // call some method here with the OpenedFile containing
  *       // the original File and the resulting SBMLDocument.
  * 		} else {
  * 			// The user has canceled the reading or it was not successful.
@@ -78,112 +78,113 @@ import de.zbit.util.Timer;
  * @version $Rev$
  */
 public class SBMLReadingTask extends SwingWorker<SBMLDocument, Void> {
-	
-	/**
-	 * For localization support.
-	 */
-	private static final transient ResourceBundle bundle = ResourceManager.getBundle("de.zbit.sbml.locales.Messages");
-	
-	/**
-	 * Key used to notify listeners about success. 
-	 */
-	public static final String SBML_READING_SUCCESSFULLY_DONE = "SBML_READING_SUCCESSFULLY_DONE";
-	
-	/**
-	 * The stream from which the SBML content is to be read.
-	 */
-	private InputStream inputStream;
-	
-	/**
-	 * The element from the graphical user interface that invokes this task. 
-	 */
-	private Component parent;
-	
-	/**
-	 * Monitors the reading operation to the user.
-	 */
-	private ProgressMonitor progressMonitor;
-
-	/**
-	 * Memorize the given SBML file for later access, e.g., in case of successful
-	 * reading.
-	 */
-	private File sbmlFile;
-	
-	/**
-	 * 
-	 * @param sbmlFile
-	 * @param parent
-	 * @throws FileNotFoundException
-	 */
-	public SBMLReadingTask(File sbmlFile, Component parent) throws FileNotFoundException {
-		super();
-		this.parent = parent;
-		this.sbmlFile = sbmlFile;
-		ProgressMonitorInputStream pmis = new ProgressMonitorInputStream(parent,
-			MessageFormat.format(bundle.getString("READING_SBML_FILE"), sbmlFile.getName()),
-			new FileInputStream(sbmlFile));
-		progressMonitor = pmis.getProgressMonitor();
-		this.inputStream = new BufferedInputStream(pmis);
-	}
-	
-	/**
-	 * 
-	 * @param sbmlFile
-	 * @param parent
-	 * @param changeListeners
-	 * @throws FileNotFoundException
-	 */
-	public SBMLReadingTask(File sbmlFile, Component parent, PropertyChangeListener...changeListeners) throws FileNotFoundException {
-		this(sbmlFile, parent);
-		if ((changeListeners != null) && (changeListeners.length > 0)) {
-			for (PropertyChangeListener listener : changeListeners) {
-				addPropertyChangeListener(listener);
-			}
-		}
-	}
-
-	/* (non-Javadoc)
-	 * @see javax.swing.SwingWorker#doInBackground()
-	 */
-	protected SBMLDocument doInBackground() throws XMLStreamException {
-		Logger logger = Logger.getLogger(SBMLReadingTask.class.getName());
-		Timer timer = new Timer();
-		try {
-			SBMLDocument doc = SBMLReader.read(inputStream);
-			logger.info(MessageFormat.format(bundle.getString("READING_TIME"), timer.getAndReset(false)));
-			return doc;
-		} catch (Exception exc) {
-			logger.info(MessageFormat.format(bundle.getString("CANCELING_AT_TIME"), timer.getAndReset(false)));
-			logger.log(Level.FINE, exc.getLocalizedMessage(), exc);
-			return null;
-		}
-	}
-	
-	/* (non-Javadoc)
-	 * @see javax.swing.SwingWorker#done()
-	 */
-	@Override
-	protected void done() {
-		progressMonitor.close();
-		try {
-			final OpenedFile<SBMLDocument> openedFile = new OpenedFile<SBMLDocument>(sbmlFile, get());
-			if (openedFile.isSetDocument()) {
-				openedFile.getDocument().addTreeNodeChangeListener(new SBMLfileChangeListener(openedFile));
-			}
-			firePropertyChange(SBML_READING_SUCCESSFULLY_DONE, null, openedFile);
-		} catch (InterruptedException exc) {
-			GUITools.showErrorMessage(parent, exc.getLocalizedMessage());
-		} catch (ExecutionException exc) {
-			GUITools.showErrorMessage(parent, exc.getLocalizedMessage());
-		}
-	}
-
-	/**
-	 * @return the sbmlFile
-	 */
-	public File getSBMLfile() {
-		return sbmlFile;
-	}
-
+  
+  /**
+   * For localization support.
+   */
+  private static final transient ResourceBundle bundle = ResourceManager.getBundle("de.zbit.sbml.locales.Messages");
+  
+  /**
+   * Key used to notify listeners about success.
+   */
+  public static final String SBML_READING_SUCCESSFULLY_DONE = "SBML_READING_SUCCESSFULLY_DONE";
+  
+  /**
+   * The stream from which the SBML content is to be read.
+   */
+  private InputStream inputStream;
+  
+  /**
+   * The element from the graphical user interface that invokes this task.
+   */
+  private Component parent;
+  
+  /**
+   * Monitors the reading operation to the user.
+   */
+  private ProgressMonitor progressMonitor;
+  
+  /**
+   * Memorize the given SBML file for later access, e.g., in case of successful
+   * reading.
+   */
+  private File sbmlFile;
+  
+  /**
+   * 
+   * @param sbmlFile
+   * @param parent
+   * @throws FileNotFoundException
+   */
+  public SBMLReadingTask(File sbmlFile, Component parent) throws FileNotFoundException {
+    super();
+    this.parent = parent;
+    this.sbmlFile = sbmlFile;
+    ProgressMonitorInputStream pmis = new ProgressMonitorInputStream(parent,
+      MessageFormat.format(bundle.getString("READING_SBML_FILE"), sbmlFile.getName()),
+      new FileInputStream(sbmlFile));
+    progressMonitor = pmis.getProgressMonitor();
+    inputStream = new BufferedInputStream(pmis);
+  }
+  
+  /**
+   * 
+   * @param sbmlFile
+   * @param parent
+   * @param changeListeners
+   * @throws FileNotFoundException
+   */
+  public SBMLReadingTask(File sbmlFile, Component parent, PropertyChangeListener...changeListeners) throws FileNotFoundException {
+    this(sbmlFile, parent);
+    if ((changeListeners != null) && (changeListeners.length > 0)) {
+      for (PropertyChangeListener listener : changeListeners) {
+        addPropertyChangeListener(listener);
+      }
+    }
+  }
+  
+  /* (non-Javadoc)
+   * @see javax.swing.SwingWorker#doInBackground()
+   */
+  @Override
+  protected SBMLDocument doInBackground() throws XMLStreamException {
+    Logger logger = Logger.getLogger(SBMLReadingTask.class.getName());
+    Timer timer = new Timer();
+    try {
+      SBMLDocument doc = SBMLReader.read(inputStream);
+      logger.info(MessageFormat.format(bundle.getString("READING_TIME"), timer.getAndReset(false)));
+      return doc;
+    } catch (Throwable exc) {
+      logger.info(MessageFormat.format(bundle.getString("CANCELING_AT_TIME"), timer.getAndReset(false)));
+      logger.log(Level.FINE, exc.getLocalizedMessage(), exc);
+      throw new XMLStreamException(exc);
+    }
+  }
+  
+  /* (non-Javadoc)
+   * @see javax.swing.SwingWorker#done()
+   */
+  @Override
+  protected void done() {
+    progressMonitor.close();
+    try {
+      final OpenedFile<SBMLDocument> openedFile = new OpenedFile<SBMLDocument>(sbmlFile, get());
+      if (openedFile.isSetDocument()) {
+        openedFile.getDocument().addTreeNodeChangeListener(new SBMLfileChangeListener(openedFile));
+      }
+      firePropertyChange(SBML_READING_SUCCESSFULLY_DONE, null, openedFile);
+    } catch (InterruptedException exc) {
+      GUITools.showErrorMessage(parent, exc.getLocalizedMessage());
+    } catch (ExecutionException exc) {
+      GUITools.showErrorMessage(parent, exc.getLocalizedMessage());
+    }
+  }
+  
+  /**
+   * @return the sbmlFile
+   */
+  public File getSBMLfile() {
+    return sbmlFile;
+  }
+  
 }
