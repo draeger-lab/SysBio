b4e12f72b3d83ec534ef857d73d71819d236e171
Andreas Dräger <andreas.draeger@uni-tuebingen.de>
2014-07-19 19:46:22 +0000

    Fixed a bug in LayoutDirector that would always change defined layouts for SBML. Now it only creates a dimensions object if there is none. Improved SBasePanel to cover more elements from layout.
    git-svn-id: svn://rarepos.cs.uni-tuebingen.de/SysBio/trunk@1344 e728d8a2-de87-4acf-9dc5-8dc47f4ff2f0

--- examples/de/zbit/sbml/layout/y/SVGDOMEnhancerForHierarchy.java
+++ examples/de/zbit/sbml/layout/y/SVGDOMEnhancerForHierarchy.java
@@ -12,52 +12,48 @@ import y.base.Node;
 import yext.svg.io.SVGDOMEnhancer;
 
 public class SVGDOMEnhancerForHierarchy extends SVGDOMEnhancer {
-
+  
   private Model model;
   private Map<String, Reaction> rxnNameToReaction;
-
+  
   public SVGDOMEnhancerForHierarchy(Model model) {
     super();
     this.model = model;
-    this.rxnNameToReaction = new HashMap<String, Reaction>();
-
+    rxnNameToReaction = new HashMap<String, Reaction>();
+    
     for (Reaction rxn : model.getListOfReactions()) {
       rxnNameToReaction.put(rxn.getName(), rxn);
     }
-
+    
   }
-
-
-
+  
   /* (non-Javadoc)
    * @see yext.svg.io.SVGDOMEnhancer#nodeAddedToDOM(y.base.Node, org.w3c.dom.Element)
    */
   @Override
   protected void nodeAddedToDOM(Node n1, Element element) {
-
+    
     Reaction reaction;
     if (rxnNameToReaction.get(n1.toString()) != null) {
       reaction = rxnNameToReaction.get(n1.toString());
       element.setAttribute("reaction", reaction.getId());
     }
-
+    
     super.nodeAddedToDOM(n1, element);
   }
-
-
-
+  
   /* (non-Javadoc)
    * @see yext.svg.io.SVGDOMEnhancer#edgeAddedToDOM(y.base.Edge, org.w3c.dom.Element)
    */
   @Override
   protected void edgeAddedToDOM(Edge edge, Element element) {
-
+    
     Node n1 = edge.source();
     Node n2 = edge.target();
-
+    
     Reaction reaction;
     Node n;
-
+    
     if (rxnNameToReaction.get(n1.toString()) != null) {
       reaction = rxnNameToReaction.get(n1.toString());
       n = n1;
@@ -70,11 +66,8 @@ public class SVGDOMEnhancerForHierarchy extends SVGDOMEnhancer {
       System.out.println("No reaction...?");
       element.setAttribute("reaction", "NoRxn");
     }
-
-
+    
     super.edgeAddedToDOM(edge, element);
   }
-
-
-
+  
 }
